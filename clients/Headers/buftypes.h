77a8e11d5836a8340e70f017f278184b8b42ab70
Ron Fox <fox@nscl.msu.edu>
2003-08-22 18:15:26 +0000

    Remove unused buffer type definitions and add documentation packet type definitions.

--- clients/Headers/buftypes.h
+++ clients/Headers/buftypes.h
@@ -275,15 +275,6 @@ DAMAGES.
 
 		     END OF TERMS AND CONDITIONS
 */
-/* DEC/CMS REPLACEMENT HISTORY, Element BUFTYPES.H*/
-/* *2    17-NOV-1989 13:45:11 FOX "Add user buffer types"*/
-/* *1     3-MAR-1988 16:36:53 FOX "Buffer type definitions"*/
-/* DEC/CMS REPLACEMENT HISTORY, Element BUFTYPES.H*/
-/* DEC/CMS REPLACEMENT HISTORY, Element BUFTYPES.H*/
-/* *2     8-OCT-1987 13:05:48 FOX "Add MAXBUFTYPE definitions and snapshot scalers"*/
-/* *1     8-OCT-1987 09:18:22 FOX "Buffer types include file."*/
-/* DEC/CMS REPLACEMENT HISTORY, Element BUFTYPES.H*/
-
 /*
 **++
 **  FACILITY:
@@ -303,49 +294,22 @@ DAMAGES.
 **  CREATION DATE:     8-Oct-1987
 **
 **  MODIFICATION HISTORY:
+**      @(#)buftypes.h	1.2 3/25/94 Include
 **--
 **/
+#ifndef __BUFTYPES_H
+#define __BUFTYPES_H
 
-#define DATABF	1
-#define SCALERBF  2
-#define SNAPSCBF	3						       /*2*/
-#define BEGRUNBF  11
-#define ENDRUNBF  12
-#define PAUSEBF   13
-#define RESUMEBF  14
-#define LINKLOSTBF 15
-#define MAXSYSBUFTYPE 31
-#define USERBUF1	32
-#define USERBUF2	33
-#define USERBUF3	34
-#define USERBUF4	35
-#define USERBUF5	36
-#define USERBUF6	37
-#define USERBUF7	38
-#define USERBUF8	39
-#define USERBUF9	40
-#define USERBUF10	41
-#define USERBUF11	42
-#define USERBUF12	43
-#define USERBUF13	44
-#define USERBUF14	45
-#define USERBUF15	46
-#define USERBUF16	47
-#define USERBUF17	48
-#define USERBUF18	49
-#define USERBUF19	50
-#define USERBUF20	51
-#define USERBUF21	52
-#define USERBUF22	53
-#define USERBUF23	54
-#define USERBUF24	55
-#define USERBUF25	56
-#define USERBUF26	57
-#define USERBUF27	58
-#define USERBUF28	59
-#define USERBUF29	60
-#define USERBUF30	61
-#define USERBUF31	62
-#define USERBUF32	63
-#define USERBUF33	64
-#define MAXBUFTYPE    65	/* Support up to this many buffer types	*/ /*2*/
+#define DATABF	        1
+#define SCALERBF        2
+#define SNAPSCBF	3
+#define STATEVARBF      4
+#define RUNVARBF        5
+#define PKTDOCBF        6
+#define BEGRUNBF       11
+#define ENDRUNBF       12
+#define PAUSEBF        13
+#define RESUMEBF       14
+
+#endif
+
