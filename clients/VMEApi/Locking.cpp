432415d7c68a8a5c0644db057be753c92e83bb46
Ron Fox <fox@nscl.msu.edu>
2003-10-31 17:50:04 +0000

    Added CVMEInterface::Lock and CVMEInterface::Unlock to support serialization between mmap and block transfers.

--- clients/VMEApi/Locking.cpp
+++ clients/VMEApi/Locking.cpp
@@ -275,134 +275,163 @@ DAMAGES.
 
 		     END OF TERMS AND CONDITIONS
 */
-/*!
-   Provides device/driver independent interfaces to the VME bus.
-   This is implemented as a class with wholey static members for the following
-   reasons:
-   - Class provides a namespace context to prevent global namespace polution.
-   - Class allows for future optimizations that support 'sharing' address space
-   mappings.
 
-    The basic functions we support are:
-    - Map an address space.
-    - Unmap an address space.
-    - Do a block read
-    - Do a block write.
+#include <CVMEInterface.h>
+#include <ErrnoException.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <assert.h>
+#include <string>
 
-    The address modes supported are:
-    A16/D32        - Short I/O space.
-    A24/D32        - Standard memory.
-    A32/D32        - Extended memory.
-    GEO/D32        - Geographical addressing.
-    MCST/D32       - Multicast control register access.
-    CBLT/D32       - Chained block transfer.
+// In some cases we need to define the semun union:
 
-    \note Assumptions:
-    - The goal is simplicity.
-    - There is only one driver on each system.
-    - All address spaces are open read/write.
-    - The goal is simplicity!
-
-
-*/
-/*
-  Author:
-    Ron Fox
-    NSCL
-    Michigan State University
-    East Lansing, MI 48824-1321
-    fox@nscl.msu.edu
-
-    $Header$
-
-    Change history:
-
-    $Log$
-    Revision 3.2  2003/10/31 17:50:04  ron-fox
-    Added CVMEInterface::Lock and CVMEInterface::Unlock
-    to support serialization between mmap and
-    block transfers.
-
-    Revision 3.1  2003/03/22 04:04:03  ron-fox
-    Added SBS/Bit3 device driver.
-
-    Revision 2.1  2003/02/11 16:46:44  ron-fox
-    Retag to version 2.1 to remove the weird branch I accidently made.
-
-    Revision 1.1.1.1  2003/02/05 14:04:29  ron-fox
-    Initial import of the NSCL Daq clients version 7.0-000 to sourceforge.
-
-
-    Revision 2.5  2002/11/20 16:06:59  fox
-    Add Multiple crate support
-
-    Revision 2.4  2002/11/13 18:56:26  fox
-    Add support for multiple crates
-
-    Revision 2.3  2002/10/09 11:28:17  fox
-    Add copyright/license stamp.
-
- * Revision 2.2  2002/07/02  15:13:56  fox
- * Go to 2.xx based releases (recover from client crash too).
- *
- * Revision 2.1  2002/07/02  15:08:16  fox
- * Transition to 2.1 releases
- *
- * Revision 1.1  2002/06/28  20:18:55  fox
- * Add device independent VME interfaces and add them to the
- * VMETcl facility.
- *
-*/
-
-#ifndef __VMEINTERFACE_H
-#define __VMEINTERFACE_H
-
-
-class CVMEInterface
-{
- public:
-  // Type definitions:
-
-  typedef enum {			// Addressing modes:
-    A16,
-    A24,
-    A32,
-    GEO,
-    MCST,
-    CBLT,
-    LAST,
-    ShortIO   = A16,
-    Standard  = A24,
-    Extended = A32,
-    Geographical = GEO,
-    Multicast = MCST,
-    ChainedBlock = CBLT
-  } AddressMode;
-
-  static const char* m_szDriverName; // Name of driver (e.g. NSCLBiT3).
-
-  static void* Open(AddressMode nMode,
-		    unsigned short crate = 0); //!< Open a logical device.
-  static void  Close(void* pDeviceHandle); //!< Close the logical device.
-  static void* Map(void* pDeviceHandle,
-	    unsigned long nBase, 
-	    unsigned long nBytes); //!< Map a chunk of the bus to pva.
-  static void Unmap(void* pDeviceHandle,
-	     void*  pBase,
-	     unsigned long lBytes); //!< Unmap a previously mapped section.
-
-  static int Read(void* pDeviceHandle,
-		  unsigned long nOffset,
-		  void*  pBuffer,
-		  unsigned long nBytes); //!< Block read from VME.
-  static int Write(void* pDeviceHandle,
-		   unsigned long nOffset,
-		   void* pBuffer,
-		   unsigned long nBytes); //!< Block write to vme.
-  static void Lock();
-  static void Unlock();
-
+#if defined(__GNU_LIBRARY__) && !(defined(_SEM_SEMUN_UNDEFINED))
+#else
+union semun {
+  int                val;	// SETVAL value.
+  struct semid_ds    *buf;	// IPC_STAT, IPC_SET buffer.
+  unsigned short int *array;	// Array for GETALL/SETALL
+  struct seminfo*    _buf;	// Buffer for IPC_INFO
 };
-
-
 #endif
+
+
+/*!
+   This file implements coarse grained VME locking.
+   If applications use this, the entire VME subsystem will be controlled
+   by a single lock.  
+*/
+
+static int semid = -1;		// This will be the id of the locking semaphore
+static int semkey= 0x564d4520;  // "VME " :-).
+
+/*!
+   This internal function is used to establish the semaphore:
+   - If the semaphore exists, it's id is just stored in semid.
+   - If the semaphore does not exist we try to create it O_EXCL
+     this is an attempt to deal with any timing holes that may
+     occur when two programs simultaneously attemp to create the semaphore.
+   - If the O_EXCL creation succeeds (semaphore does not exist), it is
+     given in initial value of 1 so that a single process can pass the lock
+     gate.
+   - If the O_EXCL creation fails, the process backs off for a while and
+     then does a semget for an existing  semaphore again assuming that on the 
+     second try, all initialization has been complete.
+
+   \throw   CErrnoException
+      If an error occured on any of the system calls.
+
+*/
+static void
+AttachSemaphore()
+{
+  // Retry loop in case anybody makes and then kills it:
+
+  while(1) {
+    // Try to get the id of an existing semaphore:
+
+    semid = semget(semkey, 0, 0777); // Try to map:
+    if(semid > 0) break;	     // Previously existing!!
+    if(errno != ENOENT) {
+      throw 
+	CErrnoException("AttachSemaphore - semget error unexpected");
+    }
+    // Sempahore does not exist.  Try to be the only guy to 
+    // create it:
+
+    semid = semget(semkey, 1, 0777 | IPC_CREAT | IPC_EXCL);
+    if(semid > 0) {
+      // We're the creator... initialize the sempahore, and return.
+
+      union semun data;
+      data.val = 1;
+
+      int istat = semctl(semid, 0, SETVAL, data); // Allow 1 holder
+      if(istat < 0) {
+	throw CErrnoException("AttachSemaphore - semctl error unexpected");
+      }
+
+      break;
+    }
+    if(errno != EEXIST) {
+      throw
+	CErrnoException("AttachSemaphore - semget error unexpected");
+    }
+    //   The semaphore popped into being between the initial try
+    //   to just attach it and our try to create it.
+    //   The next semget should work, but we want to give
+    //   the creator a chance to initialize the semaphore so
+    //   we don't try to take out a lock on the semaphore before
+    //   it is completely initialized:
+
+    sleep(1);
+  }
+  return;
+}
+/*!
+    Lock the semaphore.  If the semid is -1, the
+    semaphore is created first.  
+
+    \throw CErrnoException
+       - Really from AttachSemaphore
+       - From failures in semop.
+*/
+void 
+CVMEInterface::Lock() 
+{
+  // If necessary, get the semaphore id..
+
+  if (semid == -1) AttachSemaphore();
+  assert(semid >= 0);		// Otherwise attach.. throws.
+
+  struct sembuf buf;
+  buf.sem_num = 0;		// Only one semaphore.
+  buf.sem_op  = -1;		// Want to take the semaphore.
+  buf.sem_flg = SEM_UNDO;	// For process exit.
+
+  while (1) {			// In case of signal...
+    int stat = semop(semid, &buf, 1);
+
+    if(stat == 0) break;
+
+    if(errno != EINTR) {	// Bad errno:
+      throw CErrnoException("CVMEInterface::Lock semop gave bad status");
+    }
+    // On EINTR try again.
+  }
+  return;
+}
+/*!
+  Unlock the semaphore. It is a crime to unlock the semaphore if it doesn
+  not yet exist, since that would be unlocking a semaphore that is not yet
+  locked.
+  
+  \throw  CErrnoException
+     If the semop operation produced an error.
+  \throw string
+     If the semaphore did not yet exist.
+*/
+void
+CVMEInterface::Unlock()
+{
+  if(semid == -1) {
+    throw string("Attempt to unlock the semaphore before it was created");
+  }
+  struct sembuf buf;
+  buf.sem_num = 0;
+  buf.sem_op  = 1;
+  buf.sem_flg= SEM_UNDO;	// Undoes the locking undo.
+
+  while(1) {			// IN case of signal though not likely.
+    int stat = semop(semid, &buf, 1);
+    if(stat == 0) break;	// Got the job done!!
+    if(errno != EINTR) {
+      throw CErrnoException("CVMEInterface::Unlock semop gave bad status");
+    }
+    // on EINTR try again.
+  }
+  return;
+}
