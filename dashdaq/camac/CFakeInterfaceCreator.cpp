54c2fda48ae3734de1297ae43af37f08ee117257
Ron Fox <fox@nscl.msu.edu>
2006-06-06 18:13:58 +0000

    Initial addition.

---
+++ dashdaq/camac/CFakeInterfaceCreator.cpp
@@ -0,0 +1,37 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2005.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+#include <config.h>
+#include "CFakeInterfaceCreator.h"
+#include "CFakeInterface.h"
+
+#ifdef HAVE_STD_NAMESPACE
+using namespace std;
+#endif
+
+CFakeInterfaceCreator::CFakeInterfaceCreator()
+{
+}
+
+CFakeInterfaceCreator::~CFakeInterfaceCreator()
+{
+}
+
+CCAMACInterface*
+CFakeInterfaceCreator::operator()(string configuration)
+{
+  return new CFakeInterface;
+}
--- dashdaq/camac/TestFake.cpp
+++ dashdaq/camac/TestFake.cpp
@@ -2,16 +2,19 @@
 
 #include <config.h>
 #include "CFakeInterface.h"
+#include "CFakeInterfaceCreator.h"
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
 #include "Asserts.h"
 
-
+#include <string>
+using namespace std;
 
 class TestFake : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(TestFake);
   CPPUNIT_TEST(maxcrates);
   CPPUNIT_TEST(lastcrate);
+  CPPUNIT_TEST(creator);
   CPPUNIT_TEST_SUITE_END();
 
 
@@ -25,6 +28,7 @@ public:
 protected:
   void maxcrates();
   void lastcrate();
+  void creator();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(TestFake);
@@ -40,4 +44,12 @@ void TestFake::lastcrate()
   CFakeInterface interface;
 
   EQ((size_t)0, interface.lastCrate());
+}
+
+void TestFake::creator()
+{
+  CFakeInterfaceCreator c;
+  CFakeInterface* i = static_cast<CFakeInterface*>(c(string("junk")));
+  EQ((size_t)3, i->maxCrates());
+  delete i;
 }
