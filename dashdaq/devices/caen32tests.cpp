d519264350d20f94cde566560100002a608a2594
Ron Fox <fox@nscl.msu.edu>
2006-08-04 13:00:35 +0000

    Get the test framework to build

--- dashdaq/devices/caen32tests.cpp
+++ dashdaq/devices/caen32tests.cpp
@@ -1,10 +1,17 @@
 // Template for a test suite.
 
+
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
 #include "Asserts.h"
+#include <CAEN32.h>
+#include <stdint.h>
 
+using namespace std;
 
+// Change the lines below if the module moves:
+static const uint32_t base(0x80000000);
+static Warning msg("caen32tests needs a CAEN32module at base address 0x80000000");
 
 class Testname : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(Testname);
