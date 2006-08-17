f05b02f414a61775666b9a8968107a55dc4bac06
Ron Fox <fox@nscl.msu.edu>
2006-08-17 13:33:13 +0000

    Backup newly edited files.

---
+++ llnlReadout/commands/CBeginRun.cpp
@@ -0,0 +1,105 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2005.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+#include <config.h>
+#include "CBeginRun.h"
+#include <TCLInterpreter.h>
+#include <TCLInterpreterObject.h>
+#include <Globals.h>
+#include "tclUtil.h"
+#include <CAcquisitionThread.h>
+#include <CRunState.h>
+
+using std::vector;
+using std::string;
+
+static const string usage(
+"Usage:\n\
+   begin");
+
+/////////////////////////////////////////////////////////////////////////
+//////////////////////////////// Canonicals /////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+/*!
+  Construct the begin command 
+  \param interp : CTCLInterpreter&
+     Interpreter on which this command will be registered.
+*/
+CBeginRun::CBeginRun(CTCLInterepreter& interp) :
+  CTCLObjectProcessor(interp, "begin")
+{}
+/*!
+   Destructor does nothing important.
+*/
+BeginRun::~CBeginRun()
+{}
+
+/////////////////////////////////////////////////////////////////////////////
+///////////////////////////// Command execution /////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
+/*!
+   Process the begin run.
+   - Ensure that the preconditions for starting the run are met these are:
+     - The begin command has no additional command line parameters.
+     - The current run state is Idle
+   - Create the Adc/Scaler configuration.
+     - Configure it from the file stored in Globals::configurationFilename.
+     - Store that configuration in Globals::configuration
+   - Start up the readout thread to take data.
+
+   \param interp : CTCLInterpreter&
+        Interpreter that is exeuting this command.
+   \param objv : std::vector<CTCLObject>&
+        Reference to an object vector that contains the command parameters.
+*/
+int
+CBeginRun::operator()(CTCLInterpreter& interp,
+		      vector<CTCLObject>& objv)
+{
+  // Make sured all precoditions are met.
+
+  if (objv.size() != 1) {
+    tclUtil::Usage(getInterpreter(), 
+		   "Incorrect number of command parameters",
+		   objv
+		   usage);
+    return TCL_ERROR;
+  }
+
+  CRunState* pState = CRunState::getInstance();
+  if (pState->getState() != CRunState::Idle) {
+    tclUtil::Usage(getInterpreter(),
+		   "Invalid run state for begin be sure to stop the run",
+		   objv,
+		   usage);
+    return TCL_ERROR;
+  }
+  // Now we can start the run.
+
+  Globals::pConfig = new CConfiguration;
+  Globals::pConfig->processConfiguration(Globals::configureationFilename);
+
+  CAcquisitionThread* pReadout = CAcquisitionThread::getInstance();
+  pReadout->start(Globals::pUSBController,
+		  Globals::pConfig->getADCS()
+		  Globals::pConfig->getScalers());
+
+  tclUtil::setResult(getInterpreter(), string("Begin - Run started"));
+  return TCL_OK;
+}
+
