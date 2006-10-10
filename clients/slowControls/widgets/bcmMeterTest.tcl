2f3dac2e6fe5a465212db438ebf5843da5f7b67f
Ron Fox <fox@nscl.msu.edu>
2006-10-10 14:07:50 +0000

    Working versions of the meter code with some updates.

--- clients/slowControls/widgets/bcmMeterTest.tcl
+++ clients/slowControls/widgets/bcmMeterTest.tcl
@@ -3,12 +3,11 @@ set prefix /scratch/fox/daq/8.1test;    # Change this if --prefix changes.
 set libdir [file join $prefix TclLibs]
 lappend auto_path $libdir
 
-set channel B159F-C;                   # Change this for different bcm.
-
 source meter.tcl
 source bcmMeter.tcl
 
-::controlwidget::bcmMeter .bcm  -channel $channel
+::controlwidget::bcmMeter .bcm  -channel Z026L-C
+::controlwidget::bcmMeter .bcm2 -channel Z026R-C
+::controlwidget::bcmMeter .bcm3 -channel B159F-C
 
-
-pack .bcm 
+pack .bcm .bcm2 .bcm3 -side left
