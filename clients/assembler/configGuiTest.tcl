96cdba34c425d467a3d1cd7154979cf0f74f91ee
Ron Fox <fox@nscl.msu.edu>
2008-03-19 17:46:15 +0000

    Source in the dialog file o we can tes stuff involving dialogs.

--- clients/assembler/configGuiTest.tcl
+++ clients/assembler/configGuiTest.tcl
@@ -1,3 +1,5 @@
+#!/usr/bin/tclsh8.4
+
 package require Tk
 
 source snitwiz.tcl
@@ -5,7 +7,7 @@ source enterNode.tcl
 source readoutEntry.tcl
 source nodeTimingEntry.tcl
 source newNodeWizard.tcl
-
+source dialogwidget.tcl
 
 source fileMenu.tcl
 source helpMenu.tcl
@@ -13,4 +15,7 @@ source configGui.tcl
 
 createUI .
 
+
+flush stderr
+flush stdout
 
