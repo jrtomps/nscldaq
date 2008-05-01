30e763619cf514ebfccf03bf6a7df7e044973fd3
Ron Fox <fox@nscl.msu.edu>
2008-05-01 10:39:30 +0000

    initial coding of hytec support modules complete, added to make file.

--- llnlReadout/devices/CNADC2530.h
+++ llnlReadout/devices/CNADC2530.h
@@ -54,7 +54,7 @@ class CVMUSBReadoutList;
    in which the module generates histograms that can be read out....effectively making it
    a multichannel analyzer front end.
 
-   This module supports single-event list mode.
+   This module supports  list mode.
 
    Configuration parameters are:
 \verbatim
@@ -62,12 +62,12 @@ class CVMUSBReadoutList;
 Parameter        Value Type             Value meaning
 -csr             integer                Module register base
 -memory          integer                Desired event memory base.
--gate            bool                   Enable disable gate mode (the module can self trigger)
 -ipl             integer (0..7)         Interrupt priority level.  0 disables interrupts.
 -vector          integer (0..65535)     Interrupt vector.
 -lld             float(0-819.1)         Low level discriminator in mV .1mV resolution.
 -hld             float(0?..8.191)       High level discrminator in V .1V resolution.
 -events          integer                Number of events between data ready interrupts.
+                                        !!!!If not using interrupts set this to 1!!!!!!!
 
 */
 
@@ -76,7 +76,10 @@ class CNADC2530 : public CReadoutHardware
 {
 private:
   CReadoutModule*   m_pConfiguration;
-  uint16_t          m_reArmCsrValue; // Value to write to re-arm the module.
+  uint32_t          m_csr;
+  uint16_t          m_csrValue;
+  uint32_t          m_eventBase;
+  int               m_eventCount;
 
 public:
   // Class canonicals.
@@ -101,6 +104,11 @@ public:
   virtual void addReadoutList(CVMUSBReadoutList& list);
   virtual CReadoutHardware* clone() const;
 
+  // utililites:
+
+private:
+  uint16_t lldToRegister(double lld);
+  uint16_t hldToRegister(double hld);
   
 };
 
