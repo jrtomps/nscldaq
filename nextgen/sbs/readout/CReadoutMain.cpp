d9479a0c5f5b37982d4ff86ef338257f9b1d9432
Ron Fox <fox@nscl.msu.edu>
2008-10-15 13:40:24 +0000

    Add Tcl Server component for local connectors.

--- nextgen/sbs/readout/CReadoutMain.cpp
+++ nextgen/sbs/readout/CReadoutMain.cpp
@@ -14,13 +14,41 @@
 	     East Lansing, MI 48824-1321
 */
 
+#include <options.h>
+
 
 #include "CReadoutMain.h"
 #include <TCLLiveEventLoop.h>
-#include <options.h>
+#include <CTCLServer.h>
+#include <Exception.h>
+#include <CInvalidArgumentException.h>
+#include <CPortManager.h>
+#include <netdb.h>
+#include <stdlib.h>
+
 #include <iostream>
 
 using namespace std;
+
+/*!
+  Constructor just initializes member variables:
+
+*/
+CReadoutMain::CReadoutMain() :
+  m_pTclServer(0)
+{
+}
+
+/*!
+   Destructor kills off dynamic objects which ought to do any
+   port cleanup etc.
+*/
+CReadoutMain::~CReadoutMain()
+{
+  delete m_pTclServer;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
 
 /*!
    Entry point for the readout main object.
@@ -34,37 +62,60 @@ using namespace std;
 int
 CReadoutMain::operator()()
 {
-  int argc;
-  char** argv;
-  struct gengetopt_args_info   parsedArgs;
-  getProgramArguments(argc, argv);
-  cmdline_parser(argc, argv, &parsedArgs);
 
+  try {
+    int argc;
+    char** argv;
+    struct gengetopt_args_info   parsedArgs;
+    getProgramArguments(argc, argv);
+    cmdline_parser(argc, argv, &parsedArgs);
+    
+    
+    // Initialize the application;
+    // this include user initialization.
+    
+    SetupRunVariables();
+    SetupStateVariables();
+    SetupReadout();		// From derived class.
+    SetupScalers();	   	// Allowed to be null (the default).
+    
+    
+    // If asked to, create the Tcl server component and hook it into the event loop
+    
+    // Hook stdin into the event loop so we can run the event loop while processing
+    // commands from stdin:
+    
+    if (parsedArgs.port_given) {
+    startTclServer(string(parsedArgs.port_arg));
 
-  // Initialize the application;
-  // this include user initialization.
-
-  SetupRunVariables();
-  SetupStateVariables();
-  SetupReadout();		// From derived class.
-  SetupScalers();	   	// Allowed to be null (the default).
-
-
-  // If asked to, create the Tcl server component and hook it into the event loop
-
-  // Hook stdin into the event loop so we can run the event loop while processing
-  // commands from stdin:
-
-  if (parsedArgs.port_given) {
-    cerr << "Would start the tcl server on port " << parsedArgs.port_arg << endl;
+    }
+    
+    // Setup our eventloop.
+    
+    CTCLLiveEventLoop* pLoop = CTCLLiveEventLoop::getInstance();
+    pLoop->start();
   }
+  catch (CException& e) {
+    cerr << "CReadoutMain::operator() last chance exception handler caught CException object:\n";
+    cerr << e.ReasonText() << endl;
+  }
+  catch (std::string msg) {
+    cerr << "CReadoutMain::operator() last chance exception handler caught a string exception\n";
+    cerr << msg << endl;
+  }
+  catch (const char* msg) {
+    cerr << "CReadoutMain::operator() last chance exception handler caught const char* exception\n";
+    cerr << msg << endl;
 
-  // Setup our eventloop.
+  }  
+  catch (...) {
+    cerr << "CReadoutMain::operator() last chance exception handler caught  an unknown exception type\n";
+  }
+  exit(EXIT_FAILURE);
 
-  CTCLLiveEventLoop* pLoop = CTCLLiveEventLoop::getInstance();
-  pLoop->start();
-  return TCL_ERROR;
 }
+  
+///////////////////////////////////////////////////////////////////////////////////////////
 
 
 /*!
@@ -88,4 +139,48 @@ CReadoutMain::SetupStateVariables()
 void
 CReadoutMain::SetupScalers()
 {
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+/*!
+   Starts the Tcl server component. 
+   \param port - Port on which the program will listen for connections:
+   - If port is an integer this is the literal port.
+   - If port is the string "managed" the port manager is used to allocate a port
+     the application name will then be "Readout"
+   - If the port manager is any other string, we attempt to locate it in the system's
+     service database (getservbyname()), and use the resulting port if there is one.
+
+   \throw CInvalidArgumentException if the port is not valid.
+
+*/
+void
+CReadoutMain::startTclServer(string port)
+{
+  // Is port a number
+
+  char* endPtr;
+  unsigned long portNumber = strtoul(port.c_str(), &endPtr, 0);
+  int  thePort;
+  if (*endPtr == '\0') {
+    // Numerical port.
+
+    thePort = portNumber;
+  }
+  else if (port == string("managed")) {
+    CPortManager* pManager = new CPortManager(); // Ensures we hold the connection to the end.
+    thePort = pManager->allocatePort(string("Readout"));
+  }
+  else {
+    struct servent* pEntry = getservbyname(port.c_str(), "tcp");
+    if(pEntry) {
+      thePort = ntohs(pEntry->s_port);
+    }
+    else {
+      throw CInvalidArgumentException(port, "Must be a numeric port 'managed' or a known service name",
+				      "Starting tcp/tcl server component");
+    }
+  }
+  m_pTclServer = new CTCLServer(getInterpreter(), thePort);
 }
