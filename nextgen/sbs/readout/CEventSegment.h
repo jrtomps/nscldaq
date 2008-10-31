876ed3913184d2bba04712df65adc8dca0dc4a4a
Ron Fox <fox@nscl.msu.edu>
2008-10-31 13:16:14 +0000

    Good compilation on CEventSegment. Since this is a bunch of no-ops,   No testing is needed.

---
+++ nextgen/sbs/readout/CEventSegment.h
@@ -0,0 +1,50 @@
+#ifndef __CEVENTSEGMENT_H
+#define __CEVENTSEGMENT_H
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2005.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+#ifndef __COBJECT_H
+#include "CObject.h"
+#endif
+
+
+#ifndef __CRT_STDDEF_H
+#include <stddef.h>
+#ifndef __CRT_STDDEF_H
+#endif
+#endif
+
+/*!
+
+   This is an abstract base class for event segments.  An event segment reads out a
+   logical chunk of an experiment. What defines a logical chunk of an experiment is
+   up to the application designer.  The CCompoundEventSegment container allows you to
+   compose event segments hierarchically into larger chunks.  This makes it easy
+   to combine code from different detector subsystems while maintaining them 
+   separately.
+*/
+class CEventSegment : public CObject
+{
+public:
+  virtual void   initialize();
+  virtual void   clear();
+  virtual void   disable();
+  virtual size_t read(void* pBuffer, size_t maxwords) = 0;
+};
+
+
+#endif
+
