d49d3c060054a88b91e4b955132c3d4b7203e40f
Ron Fox <fox@nscl.msu.edu>
2009-03-13 19:21:02 +0000

    Tested to the extent that I can figure out how to test it.

--- nextgen/sbs/readout/docvartests.cpp
+++ nextgen/sbs/readout/docvartests.cpp
@@ -8,18 +8,27 @@
 #include <TCLObject.h>
 
 #define private public		// dirty friendliness.
+#define protected public
 #include "CDocumentedVars.h"
 #undef private
+#undef protected
+
+#include "CVarList.h"
 
 #include "RunState.h"
 #include <vector>
+
+#include <string>
 
 
 using namespace std;
 
 class DocVars : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(DocVars);
-  CPPUNIT_TEST(aTest);
+  CPPUNIT_TEST(construction);
+  CPPUNIT_TEST(create1ofeach);
+  CPPUNIT_TEST(createdups);
+  CPPUNIT_TEST(deleteExisting);
   CPPUNIT_TEST_SUITE_END();
 
 
@@ -36,10 +45,203 @@ public:
     delete m_pInterp;
   }
 protected:
-  void aTest();
+  void construction();
+  void create1ofeach();
+  void createdups();
+  void deleteExisting();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(DocVars);
 
-void DocVars::aTest() {
+/*
+  When the object is constructed:
+  - the state variable lists should be empty
+  - The statevar token shoudl be non null.
+  - The prior values map should be empty
+*/
+
+void DocVars::construction() {
+  ASSERT(m_pVars->m_StateVariables.size() == 0);
+  ASSERT(m_pVars->m_RunVariables.size() == 0);
+  ASSERT(m_pVars->m_PriorValues.size() == 0);
+  ASSERT(m_pVars->m_statevarToken);
+}
+
+/*
+  Creating one of each should put an entry in each of the variable lists and one
+  in the prior values map.  We're not going to try to build the variable itself nor give it
+  an initial value
+  prior value map entry should be a null pointer.
+*/
+void DocVars::create1ofeach()
+{
+  vector<CTCLObject> command;
+  CTCLObject         word;
+  
+  // runvar george
+
+  word = "runvar";
+  command.push_back(word);
+  word = "george";
+  command.push_back(word);
+
+  int status = (*m_pVars)(*m_pInterp, command);
+  ASSERT(status == TCL_OK);
+
+  // statevar harry
+
+  command.clear();
+  word = "statevar";
+  command.push_back(word);
+  word = "harry";
+  command.push_back(word);
+  status = (*m_pVars)(*m_pInterp, command);
+  ASSERT(status == TCL_OK);
+
+  // Now look into the state of the command object:
+
+  ASSERT(m_pVars->m_StateVariables.size() == 1);
+  ASSERT(m_pVars->m_RunVariables.size()   == 1);
+  ASSERT(m_pVars->m_PriorValues.size()    == 1);
+
+  // The george entry of the prior values should be a null pointer.
+
+  string* pPrior = m_pVars->m_PriorValues[string("george")];
+  EQ(reinterpret_cast<string*>(0), pPrior);
+}
+
+/*
+  Creating a dup will fail. For now the tests only look at the return code
+  (should be TCL_ERROR), not the result.
+*/
+void DocVars::createdups()
+{
+  vector<CTCLObject> command;
+  CTCLObject         word;
+
+  word   = "runvar";
+  command.push_back(word);
+  word  = "george";
+  command.push_back(word);
+
+  int status = (*m_pVars)(*m_pInterp, command);
+  status     = (*m_pVars)(*m_pInterp, command);
+
+  EQMSG( " Duplicate runvar", TCL_ERROR, status);
+
+  // runvar/statevar are in the same namespace so:
+
+  word = "statevar";
+  command[0] = word;
+  status = (*m_pVars)(*m_pInterp, command);
+  EQMSG( "statevar same name as runvar.", TCL_ERROR, status);
+
+  // Statevar should not tolerate dups either.
+
+  word = "harry";
+  command[1] = word;
+  status = (*m_pVars)(*m_pInterp, command);
+  status = (*m_pVars)(*m_pInterp, command);
+  EQMSG( "statevar duplicate", TCL_ERROR, status);
+
+  word = "runvar";
+  status = (*m_pVars)(*m_pInterp, command);
+  EQMSG( "runvar same as statevar", TCL_ERROR, status);
+}
+/*
+ * Ensure that:
+ *  - delete existing works.
+ *  - delete existing requires the right command to delete one.
+ */
+void DocVars::deleteExisting()
+{
+  CTCLObject word;
+  vector<CTCLObject> command;
+  // runvar george
+  
+  word   = "runvar";
+  command.push_back(word);
+  word  = "george";
+  command.push_back(word);
+  int status = (*m_pVars)(*m_pInterp, command);
+  
+  // runvar -delete george
+
+  word = "-delete";
+  command[1] = word;
+  word = "george";
+  command.push_back(word);
+  status = (*m_pVars)(*m_pInterp, command);
+  EQMSG("runvar -delete george", TCL_OK, status);
+
+  // look at commando object internals:
+
+  ASSERT(m_pVars->m_RunVariables.size() == 0);
+  ASSERT(m_pVars->m_PriorValues.size() == 0);
+
+  // Recreate george and attempt to delete it as a statevar:
+
+  command.clear();
+  word   = "runvar";
+  command.push_back(word);
+  word  = "george";
+  command.push_back(word);
+  status = (*m_pVars)(*m_pInterp, command);
+  EQMSG("recreating runvar george", TCL_OK, status);
+  
+  word = "statevar";
+  command[0] = word;
+  word = "-delete";
+  command[1] = word;
+  word = "george";
+  command.push_back(word);
+  status = (*m_pVars)(*m_pInterp, command);
+  EQMSG("deleting runvar as statevar", TCL_ERROR, status);
+  
+  word = "runvar";
+  command[0] = word;
+  (*m_pVars)(*m_pInterp, command); // actually kill the var.
+
+  // Do all this with statevars too:
+
+  command.clear();
+  word   = "statevar";
+  command.push_back(word);
+  word  = "george";
+  command.push_back(word);
+  status = (*m_pVars)(*m_pInterp, command);
+  
+  // statevar -delete george
+
+  word = "-delete";
+  command[1] = word;
+  word = "george";
+  command.push_back(word);
+  status = (*m_pVars)(*m_pInterp, command);
+  EQMSG("runvar -delete george", TCL_OK, status);
+
+  // look at commando object internals:
+
+  ASSERT(m_pVars->m_StateVariables.size() == 0);
+  ASSERT(m_pVars->m_PriorValues.size() == 0);
+
+  // Recreate george and attempt to delete it as a runvar.
+
+  command.clear();
+  word   = "statevar";
+  command.push_back(word);
+  word  = "george";
+  command.push_back(word);
+  status = (*m_pVars)(*m_pInterp, command);
+  EQMSG("recreating runvar george", TCL_OK, status);
+  
+  word = "runvar";
+  command[0] = word;
+  word = "-delete";
+  command[1] = word;
+  word = "george";
+  command.push_back(word);
+  status = (*m_pVars)(*m_pInterp, command);
+  EQMSG("deleting statevar as runvar", TCL_ERROR, status);
+  
 }
