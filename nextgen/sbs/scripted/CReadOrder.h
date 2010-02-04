e8f28738aa81d6818b88fb39fec0a2e99a14c122
Ron Fox <fox@nscl.msu.edu>
2010-02-04 14:14:44 +0000

    Fixes to get this to build from an SVN checkout

--- nextgen/sbs/scripted/CReadOrder.h
+++ nextgen/sbs/scripted/CReadOrder.h
@@ -64,6 +64,13 @@ using namespace std;
 #define __TCL_H
 #endif
 
+#ifndef __CRT_STDINT_H
+#include <stdint.h>
+#ifndef __CRT_STDINT_H
+#define __CRT_STDINT_H
+#endif
+#endif
+
 // Forward Definitions:
 
 class CTCLInterpreter;
