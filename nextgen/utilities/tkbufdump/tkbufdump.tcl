13c3946128d863802c8d78053f76d01f779ff3e9
Ron Fox <fox@nscl.msu.edu>
2010-02-12 22:14:24 +0000

    Got the ability to read data from the input source and display a simple hex dump on the dump window.

--- nextgen/utilities/tkbufdump/tkbufdump.tcl
+++ nextgen/utilities/tkbufdump/tkbufdump.tcl
@@ -11,7 +11,9 @@ package require BWidget
 
 set here [file dirname [info script]]
 
-source [file join $here dataSourcePrompter.tcl]
+source [file join $here datasource.tcl]
+
+set daqbin /usr/opt/daq/10.0/bin
 
 #  mainwindow
 #
@@ -34,10 +36,22 @@ source [file join $here dataSourcePrompter.tcl]
 snit::widget mainwindow {
     option -statustext {}
     option -maxlines   200
-    #
-    #   User event handlers:
-    #
     
+    # Some saved widgets.
+    
+    variable statusbar
+    variable text
+    
+    # File descriptor open on pipe to event source.
+    
+    variable fd {}
+
+    # This variable is set to 1 when the user requests an event from
+    # the data source.  It will be reset when the even has
+    # been selected and displayed (selection implies matching any filter
+    # criteria that are in place).
+    #
+    variable sample 0
     
     constructor {args} {
 
@@ -66,17 +80,18 @@ snit::widget mainwindow {
 	# 25 lines by 80 characters wide.
 	#
 	
-	text      $win.dump      -width 80 -height 25 -relief ridge \
-	                         -yscrollcommand [list $win.yscroll set] \
-				 -state disabled
+	set text [
+	    text      $win.dump      -width 80 -height 25 -relief ridge \
+				     -yscrollcommand [list $win.yscroll set] \
+				     -state disabled]
 	
 	scrollbar $win.yscroll   -command [list $win.dump yview] -orient vertical
 
 	grid $win.dump  $win.yscroll -sticky nsew
 
 	frame $win.bottom -relief ridge
-	label $win.bottom.statusbar
-	ArrowButton $win.bottom.next -dir right
+	set statusbar [label $win.bottom.statusbar]
+	ArrowButton $win.bottom.next -dir right -command [mymethod requestSample]
 	
 	
 	pack $win.bottom.statusbar -side left
@@ -100,15 +115,15 @@ snit::widget mainwindow {
     #  Insert a line of text.
     #
     method add line {
-	$win.dump configure -state normal
-	$win.dump insert end "$line\n"
+	$text configure -state normal
+	$text insert end "$line\n"
 	
-	set lines [$win.dump count -lines 1.0 end]
+	set lines [lindex [split [$text index end] .] 0]
 	if {$lines > $options(-maxlines)} {
-	    $win.dump delete 1.0 2.0
+	    $text delete 1.0 2.0
 	}
-	$win.dump see end
-	$win.dump configure -state disabled
+	$text see end
+	$text configure -state disabled
     }
     #
     #   Adds multiple lines of text:
@@ -136,7 +151,75 @@ snit::widget mainwindow {
     #               to accept data.
     #
     method openSource {} {
+	dataSourcePrompt .p -okcommand [list .p dismiss] -cancelcommand [list destroy .p]
+	.p modal
+	#
+	#  If .p does not exist it was cancelled, otherwise we can fetch the
+	#  host and the username from the widget:
+	#
 	
+	if {[winfo exists .p]} {
+	    if {$fd ne ""} {
+		close $fd
+	    }
+	    set host [.p cget -host]
+	    set user [.p cget -user]
+	    
+	    $statusbar configure -text "Data from $host:$user"
+	    
+	    destroy .p
+	    
+	    set fd [open "|$::daqbin/ringselector --formatted --sample=PHYSICS_EVENT" r]
+	    fconfigure $fd -buffering line
+	    fileevent $fd readable [mymethod event $fd]
+	}
+    }
+    #
+    # Takes the current event and matches it against the filter criteria
+    # returns 1 for a match 0 for no match
+    # Paramters:
+    #   line - ASCII-zed event as a tcl list.
+    #
+    method matchFilter {line} {
+	return 1;                                   # Stub.
+    }
+    #  Formats an event and adds it to the output window
+    # Parameters:
+    #    event   - ASCII-zed event as a tcl list.
+    #
+    method formatEvent event {
+	$self add "---------------------"
+	foreach word $event {
+	    append line [format "%04x " $word]
+	}
+	$self add $line
+    }
+    
+    # event - Called whenthe data source becomes readable.  This will happen
+    #         either as a result of an event, in which case the event is read,
+    #         or due to the data source being closed.
+    # Parameters:
+    #   The fd that was readable.
+    #
+    method event {f} {
+	if {[eof $f]} {
+	    close $f
+	    return
+	}
+	gets $f line
+	if {$sample} {
+	    if {[$self matchFilter $line]} {
+		$self formatEvent $line
+		set sample 0
+	    }
+	}
+    }
+    #
+    # requestSample - Request the next event that matches the filter criteria
+    #                 be displayed on the text widget.
+    #
+    method requestSample {} {
+	set sample 1
     }
 }
 
