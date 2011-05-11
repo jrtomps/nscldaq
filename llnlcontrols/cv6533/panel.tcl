05656d47a3df7dddc125d8ff5f227c28dedf2c28
Ron Fox <fox@nscl.msu.edu>
2011-05-11 00:12:24 +0000

    Added row labels for the widgets.

--- llnlcontrols/cv6533/panel.tcl
+++ llnlcontrols/cv6533/panel.tcl
@@ -12,6 +12,54 @@ package require Tk
 
 set here [file dirname [info script]]
 source [file join $here channel.tcl]
+#
+#  Print the program usage:
+#
+
+proc usage {} {
+    puts stderr "Usage:"
+    puts stderr "   panel name"
+    puts stderr "Where:"
+    puts stderr "   name - the name of a V6533 device in controlconfig.tcl"
+
+}
+
+# 
+#  Produce a row label. This serves to 
+#  describe the contents of the channel widgets:
+#
+# Parameters:
+#   wid  - widget name of containing frame.
+# Returns:
+#   wid
+#
+proc rowLabel wid {
+    frame $wid -relief groove -borderwidth 2
+    label $wid.name -text name
+    label $wid.set  -text setpoint
+    label $wid.v    -text voltage
+    label $wid.i    -text current
+
+    grid $wid.name
+    grid $wid.set
+    grid $wid.v
+    grid $wid.i
+
+    return $wid
+}
+
+#------------------------------------------------
+
+#  If the parameter count is not right, error exit
+
+if {[llength $argv] != 1} {
+    usage
+    exit -1
+}
+set name $argv
+
+label .l -text "V6533 control for $argv"
+grid .l -columnspan 3
 
 #  Layout the main part of the screen:
 #  there are channel widgets for each of the
@@ -19,18 +67,22 @@ source [file join $here channel.tcl]
 #
 
 for {set i 0} {$i < 6} {incr i} {
-    v6533Channel .c$i
+    v6533Channel .c$i -label "Ch $i" -blabel On -bg green \
+	-setpoint 0 -actualv 0 -actuali 0
 
 }
 #  Each row has three widgets:
 
-set row 0
-set col 0
+set row 1
+set col 1
+grid [rowLabel .r$row] -row $row -column 0 -sticky n
 for {set i 0} {$i < 6} {incr i} {
     grid .c$i -row $row  -column $col
     incr col
     if {$col > 2} {
-	set col 0
+	set col 1
 	incr row
+	grid [rowLabel .r$row] -row $row -column 0 -sticky n
+
     }
 }
