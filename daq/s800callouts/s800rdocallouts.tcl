0e828e97c1eed67958124d0de5c38c030a94af06
Ron Fox <fox@nscl.msu.edu>
2012-05-22 19:48:40 +0000

    - Start s800 pipeline in callouts. - Provide data formatting docs.. - Doc BufferToRing

--- daq/s800callouts/s800rdocallouts.tcl
+++ daq/s800callouts/s800rdocallouts.tcl
@@ -25,6 +25,7 @@ puts "---------------------- S800 readout callouts script -------------"
 # The assumption is that the s800 package is in our search path:
 #
 package require s800
+package require ReadoutGUIPanel
 
 namespace eval s800 {
     variable initialized false;	# If initialization has not taken place.
@@ -32,73 +33,13 @@ namespace eval s800 {
     variable record
     variable unexpectedEnd [list]; # Command to execute on unexpected run end.
     variable runState      {unknown}
+    variable pipelineRunning 0;	# 'true' if the catcher/feeder pipeline is running.
+    variable here          [file dirname [info script]]; # Locate ourself.
+    variable s800Host      spdaq48;			 #  Default s800 spdaq
+    variable s800Port      9002;			 # Default s800 port.
+    variable s800Ring      s800;			 # Default s800 destination ring
 }
 
-#
-#  This should be explicitly called to initialize thec connection with
-#  the remote run control system.
-#
-# @param host - host running the s800 readout.
-# @param port - Port on which it listens for run control connections;
-#
-# @note - The s800 is put in to slave mode.
-# @note - exit is renamed to s800::exit and 
-#         1. exit is defined here to set the server to master mode
-#            and close the connection prior to doing an s800::exitr
-#         2. has a destroy handler set to do the same thing.
-proc s800::Initialize {{host localhost} {port 8000}} {
-    if {[catch {
-
-    set s800::s800 [s800rctl %AUTO% -host $host -port $port]
-    
-
-    # Set the S800 in slave mode and, if it is running stop it to match
-    # the current GUI state:
-
-
-
-    set state [$::s800::s800 getState]
-    if {$state eq "active"} {
-	tk_messageBox -title  {s800 run active} \
-	    -message {The S800 run is active and must be stopped first}
-	::exit -1
-    }
-    $::s800::s800 setSlave; 
-} msg]} {
-	tk_messageBox -title {S800 protocol error} -message "Error communicating with S800@$host: $msg"
-	::exit -1
-    }
-
-    # Remove the pause/resume button....once the GUI has time to start.
-
-    after 1000 [list if {[winfo exists .pausres]} {grid forget .pauseres}]
-    
-    #  Set destroy handler:
-    
-    bind . <Destroy> [list s800::DestroyHandler %W]
-    
-    # Rename exit.
-    
-    rename ::exit ::s800::real_exit
-    rename ::s800::exit ::exit
-    
-    toplevel .s800
-    checkbutton .s800.record -text {Record Crate files} -variable ::s800::record \
-	-onvalue 1 -offvalue 0
-    pack .s800.record
-
-    # Status data for the s800:
-
-    label .s800statel -text {S800 state: }
-    label .s800state  -textvariable ::s800::runState
-
-    grid .s800statel -row 12 -column 5 -columnspan 2
-    grid .s800state  -row 12 -column 7
-
-    s800::monitorState 2000;	# Monitor th s800 state every nms.
-    
-    
-}
 
 ##
 # Self rescheduling proc that monitors the state of the S800.
@@ -178,10 +119,135 @@ proc s800::exit {{status 0}} {
     after 100 [list ::s800::real_exit $status]
 }
 
+## Get a value from its default and env name.
+#
+#  @param default - default value.
+#  @param envName - Name of env variable that overrides.
+# 
+# @return value - the final value.
+#
+proc s800::getValue {default envName} {
+    if {[array names ::env $envName] eq "" } {
+	return $default
+    }
+    return $::env($envName)
+}
+
+##
+#  If the feeder pipeline gets readable, we read 
+#  if the pipe got closed, close our end and set piplineRunning false.
+#
+# @param fd - File descriptor open on the pipe.
+#
+proc s800::pipeReadable fd {
+    if {[eof $fd]} {
+	close $fd
+	set ::s800::pipelineRunning 0
+    } else {
+	ReadougGUIPanel::outputText "s800: [gets $fd]"
+    }
+} 
+
+##
+# Start the s800 catcher/ring feeder process.
+#
+proc s800::_startFeeder {} {
+    set host [::s800::getValue $::s800::s800Host S800_HOST]
+    set port [::s800::getValue  $::s800::s800Port S800_PORT]
+    set ring [::s800::getValue $::s800::s800Ring  S800_RING]
+
+    set bindir [file join $::s800::here .. .. bin]
+    
+    set fd [open "| [file join $bindir s800toring] $host $port $ring 2>@1" r]
+    set ::s800::pipelineRunning 1
+
+    # Setup a filevent to catch output/errors and close:
+
+    fconfigure $fd -buffering line
+    fileevent $fd readable [list ::s800::pipeReadable  $fd]
+
+}
+
+
+#------------------------------------------------------------------
+#
+#  This should be explicitly called to initialize thec connection with
+#  the remote run control system.
+#
+# @param host - host running the s800 readout.
+# @param port - Port on which it listens for run control connections;
+#
+# @note - The s800 is put in to slave mode.
+# @note - exit is renamed to s800::exit and 
+#         1. exit is defined here to set the server to master mode
+#            and close the connection prior to doing an s800::exitr
+#         2. has a destroy handler set to do the same thing.
+proc s800::Initialize {{host localhost} {port 8000}} {
+    if {[catch {
+
+    set s800::s800 [s800rctl %AUTO% -host $host -port $port]
+    
+
+    # Set the S800 in slave mode and, if it is running stop it to match
+    # the current GUI state:
+
+
+
+    set state [$::s800::s800 getState]
+    if {$state eq "active"} {
+	tk_messageBox -title  {s800 run active} \
+	    -message {The S800 run is active and must be stopped first}
+	::exit -1
+    }
+    $::s800::s800 setSlave; 
+} msg]} {
+	tk_messageBox -title {S800 protocol error} -message "Error communicating with S800@$host: $msg"
+	::exit -1
+    }
+
+    # Remove the pause/resume button....once the GUI has time to start.
+
+    after 1000 [list if {[winfo exists .pausres]} {grid forget .pauseres}]
+    
+    #  Set destroy handler:
+    
+    bind . <Destroy> [list s800::DestroyHandler %W]
+    
+    # Rename exit.
+    
+    rename ::exit ::s800::real_exit
+    rename ::s800::exit ::exit
+    
+    toplevel .s800
+    checkbutton .s800.record -text {Record Crate files} -variable ::s800::record \
+	-onvalue 1 -offvalue 0
+    pack .s800.record
+
+    # Status data for the s800:
+
+    label .s800statel -text {S800 state: }
+    label .s800state  -textvariable ::s800::runState
+
+    grid .s800statel -row 12 -column 5 -columnspan 2
+    grid .s800state  -row 12 -column 7
+
+    s800::monitorState 2000;	# Monitor th s800 state every nms.
+    
+    
+}
+
 ##
 # onBegin - start the s800 run.
 #
 proc s800::OnBegin {} {
+
+    # If necessary, start the feeder pipline assumed to be in 
+    # here/../../bin
+
+    if {!$::s800::pipelineRunning} {
+	::s800::_startFeeder
+    }
+
     set title [ReadoutState::getTitle]
     set run   [ReadoutState::getRun]
     set home  $::env(HOME)
