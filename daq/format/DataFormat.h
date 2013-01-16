d3291b23a7f4a0e9300da38c06da492edbb6e2ea
Ron Fox <fox@nscl.msu.edu>
2013-01-16 15:44:09 +0000

    10.1-100 - Handle timestamped non incr scaler items -- s800 will timestamp scalers so that e.g. Gretina can properly order them.

--- daq/format/DataFormat.h
+++ daq/format/DataFormat.h
@@ -76,6 +76,7 @@ static const uint32_t MONITORED_VARIABLES = 11;
 // Scaler data:
 
 static const uint32_t INCREMENTAL_SCALERS = 20;
+static const uint32_t TIMESTAMPED_NONINCR_SCALERS =21;
 
 // Physics events:
 
@@ -134,10 +135,27 @@ typedef struct _ScalerItem {
   RingItemHeader  s_header;
   uint32_t        s_intervalStartOffset;
   uint32_t        s_intervalEndOffset;
-  uint32_t          s_timestamp;
+  uint32_t        s_timestamp;
   uint32_t        s_scalerCount;
   uint32_t        s_scalers[1];
 } ScalerItem, *pScalerItem;
+
+/*
+  Nonincremental timestamped scalers are scalers that dont' get 
+  reset on reads and have a 64 bit timestamp.  The timing information
+  put in those scalers includes both the raw time and a divisor that
+  can be used to support sub-second timing information
+*/
+typedef struct _NonIncrTimestampedScaler {
+  RingItemHeader  s_header;
+  uint64_t        s_eventTimestamp; /* For event building. */
+  uint32_t        s_intervalStartOffset;
+  uint32_t        s_intervalEndOffset;
+  uint32_t        s_intervalDivisor;
+  uint32_t        s_clockTimestamp; /* unix time */
+  uint32_t        s_scalerCount;
+  uint32_t        s_scalers[1];
+} NonIncrTimestampedScaler, *pNonIncrTimestampedScaler;
 
 /*!
   The various documentation Events are just a bunch of null terminated strings that
@@ -151,6 +169,7 @@ typedef struct _TextItem {
   uint32_t       s_stringCount;
   char           s_strings[1];
 } TextItem, *pTextItem;
+
 
 /*!
   For now a physics event is just a header and a body of uint16_t's.
@@ -186,6 +205,10 @@ extern "C" {
   pPhysicsEventCountItem formatTriggerCountItem(uint32_t runTime, time_t stamp, uint64_t triggerCount);
   pScalerItem         formatScalerItem(unsigned scalerCount, time_t timestamp, 
 				      uint32_t btime, uint32_t etime, void* pCounters);
+  pNonIncrTimestampedScaler  formatNonIncrTSScalerItem(unsigned scalerCount, time_t timestamp, 
+						       uint32_t btime, uint32_t etime, 
+						       uint64_t eventTimestamp, void* pCounters,
+						       uint32_t timebaseDivisor);
   pTextItem          formatTextItem(unsigned nStrings, time_t stamp, uint32_t runTime,
 				    const char** pStrings, int type);
   pStateChangeItem   formatStateChange(time_t stamp, uint32_t offset, uint32_t runNumber,
