1b451941e0f51f639045dd292d6db4ee4142d46f
Jeromy Tompkins <tompkins@nscl.msu.edu>
2014-01-28 18:29:34 +0000

    Adds and updates copyright info for some files in daq/format directory

--- daq/format/CDataSourceFactory.cpp
+++ daq/format/CDataSourceFactory.cpp
@@ -1,6 +1,6 @@
 /*
     This software is Copyright by the Board of Trustees of Michigan
-    State University (c) Copyright 2009.
+    State University (c) Copyright 2014.
 
     You may use this software under the terms of the GNU public license
     (GPL).  The terms of this license are described at:
@@ -9,6 +9,7 @@
 
      Author:
              Ron Fox
+             Jeromy Tompkins
 	     NSCL
 	     Michigan State University
 	     East Lansing, MI 48824-1321
--- daq/format/datasourcefactorytests.cpp
+++ daq/format/datasourcefactorytests.cpp
@@ -1,3 +1,21 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2014.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Jeromy Tompkins 
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+
+static const char* Copyright = "(C) Copyright Michigan State University 2014, All rights reserved";
 #include <fstream>
 #include <cppunit/extensions/HelperMacros.h>
 
