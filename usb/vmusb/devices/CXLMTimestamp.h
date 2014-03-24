212e140ca97e729830eec0a494b2c96a4fc24599
Jeromy Tompkins <tompkins@nscl.msu.edu>
2014-03-24 11:37:25 +0000

    Adds the CXLMTimestamp driver for Daniel Bazin's XLM tstamp firmware

---
+++ usb/vmusb/devices/CXLMTimestamp.h
@@ -0,0 +1,30 @@
+
+#ifndef CXLMTIMESTAMP_H
+#define CXLMTIMESTAMP_H
+
+#include <CXLM.h>
+
+class CXLMTimestamp : public CXLM
+{
+
+public:
+  CXLMTimestamp();
+  CXLMTimestamp(const CXLMTimestamp& rhs);
+  virtual ~CXLMTimestamp();
+
+private:
+  CXLMTimestamp& operator=(const CXLMTimestamp& rhs); // assignment not allowed.
+  int operator==(const CXLMTimestamp& rhs) const;	  // Comparison for == and != not suported.
+  int operator!=(const CXLMTimestamp& rhs) const;
+
+
+public:
+  virtual void onAttach(CReadoutModule& configuration);
+  virtual void Initialize(CVMUSB& controller);
+  virtual void addReadoutList(CVMUSBReadoutList& list);
+  virtual CReadoutHardware* clone() const; 
+
+};
+
+
+#endif
