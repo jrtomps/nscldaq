aa3229599cc8c4942844ddf350316d48ce7352ea
Jeromy Tompkins <tompkins@nscl.msu.edu>
2014-05-16 13:05:11 +0000

    Adds COneShotHandler to encapsulate the expected functionality of the --oneshot operation

---
+++ utilities/filter/COneShotHandler.h
@@ -0,0 +1,38 @@
+
+
+#ifndef STATECHANGEHANDLER_H
+#define STATECHANGEHANDLER_H
+
+#include <stdint.h>
+#include <map>
+
+class CRingStateChangeItem;
+
+class COneShotHandler 
+{
+  private:
+  unsigned int m_nExpectedSources;
+  std::map<uint32_t,uint32_t> m_stateCounts;
+  uint32_t m_cachedRunNo;
+  bool     m_complete;
+
+  public:
+    COneShotHandler(unsigned int ntrans);
+
+    void initialize(CRingStateChangeItem* item);
+
+    void update(CRingStateChangeItem* item);
+
+    bool waitingForBegin() const;
+    bool complete() const;
+
+    void reset();
+
+    uint32_t getCount(uint32_t key) const;
+
+  private:
+    bool validType(uint32_t type) const;
+    void clearCounts();
+    
+};
+#endif
