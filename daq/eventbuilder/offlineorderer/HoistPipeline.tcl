2ee25d2a70dad7eed29acc2d77b0f6366cc15a67
Jeromy Tompkins <tompkins@nscl.msu.edu>
2014-09-09 20:53:35 +0000

    Validate methods pass list by reference

--- daq/eventbuilder/offlineorderer/HoistPipeline.tcl
+++ daq/eventbuilder/offlineorderer/HoistPipeline.tcl
@@ -19,22 +19,24 @@ snit::type OfflineEVBHoistPipeParams {
   method validate {} {
      set errors [list]
 
-     set ringErr      [$self validateSourceRing $errors]
-     set tstampErr    [$self validateTstampLib $errors]
-     set idErr        [$self validateId $errors]
-     set infoErr      [$self validateInfo $errors]
+     $self validateSourceRing errors
+     $self validateTstampLib errors
+     $self validateId errors
+     $self validateInfo errors
 
      return $errors
 
   }
 
-  method validateSourceRing {errors} {
+  method validateSourceRing {errors_} {
+    upvar $errors_ errors
     if {! [file exists [file join /dev shm $options(-sourcering)]]} {
        lappend errors "Ring $options(-sourcering) does not exist on localhost."
     }
   }
 
-  method validateTstampLib {errors} {
+  method validateTstampLib {errors_} {
+    upvar $errors_ errors
     if {$options(-tstamplib) eq "" } {
        lappend errors "Timestamp extractor library has not been specified and is mandatory."
     } elseif {! [file exists $options(-tstamplib)] } {
@@ -43,13 +45,15 @@ snit::type OfflineEVBHoistPipeParams {
   }
 
 
-  method validateId {errors} {
+  method validateId {errors_} {
+    upvar $errors_ errors
     if {$options(-id) < 0} {
       lappend errors "Source id for ringFragmentSource must be greater than or equal to 0. User specified $options(-id)."
     }
   }
 
-  method validateInfo {errors} {
+  method validateInfo {errors_} {
+    upvar $errors_ errors
     if {$options(-info) eq ""} {
       lappend errors "Source info for ringFragmentSource has not been defined."
     }
