3521f50175e2d5aa33bb84fba7f1f446ece186ec
Jeromy Tompkins <tompkins@nscl.msu.edu>
2014-09-24 20:09:52 +0000

    Documents input pipeline and alters some tests

--- daq/eventbuilder/offlineorderer/InputPipeline.tcl
+++ daq/eventbuilder/offlineorderer/InputPipeline.tcl
@@ -6,17 +6,35 @@ package require InstallRoot
 package require ring
 
 
+## @brief An object encapsulating the parameters used in the input pipeline
+#
+# The parameters in that are controlled by this object are used in creating
+# pipeline composed of :
+#   cat files | unglom unglomid | frag2ring --strip | stdintoring inputring
+# 
+# The InputPipeline uses this for launching an input pipeline of the above type.
+# 
 snit::type OfflineEVBInputPipeParams {
 
-  option -file      -default ""
-  option -unglomid  -default 0
-  option -inputring -default "OfflineEVBIn"
+  option -file      -default ""             ;#< A list of files
+  option -unglomid  -default 0              ;#< labels items w/out body headers
+  option -inputring -default "OfflineEVBIn" ;#< ring to dump data into
 
+  ##  @brief a basic constructor
+  # 
+  # Configures the list
+  #
+  # @param args   option-value pairs
   constructor {args} {
     $self configurelist $args
   }
 
-
+  ## @brief Validate the options
+  #
+  # Passes a list around to a number of different validation mehtods that each
+  # append any error messages that they might find.
+  #
+  # @returns list of errors
   method validate {} {
     
     set errors [list]
@@ -29,13 +47,22 @@ snit::type OfflineEVBInputPipeParams {
   }
 
 
+  ## @brief Valdiate the list of files provides
+  #
+  # Checks whether any input files were even specified and whether they exist
+  # if they have been specified.
+  #
+  # @param errors_  variable name of list
+  #
   method validateFiles {errors_} {
     upvar $errors_ errors
 
+    # check for presence of input files
     if {[llength $options(-file)] == 0 } {
       lappend errors "No input file(s) provided."
     }
 
+    # check for existences of any that have been provided
     foreach file $options(-file) {
       if {![file exists $file]} {
         lappend errors "$file does not exist."
@@ -45,25 +72,48 @@ snit::type OfflineEVBInputPipeParams {
   }
 
 
+  ## @brief Validate the unglom id
+  #
+  # Merely ensure that it is a non-negative integer.
+  #
+  # @param errors_  variable name of list
+  #
   method validateUnglomId {errors_} {
     upvar $errors_ errors
 
     if {$options(-unglomid) < 0} {
-      lappend errors "unglom source id option must be greater than or equal to zero. User has provided $options(-unglomid)"
+      set msg "unglom source id option must be greater than or equal "
+      append msg "to zero. User has provided $options(-unglomid)"
+      lappend errors $msg
     }
 
   }
 
+  ## @brief Validate the input ring
+  #
+  # Check that the the specified ring exists on localhost
+  #
+  # @param errors_  variable name of list
+  #
   method validateInputRing {errors_} {
     upvar $errors_ errors
 
     if {! [file exists [file join /dev shm $options(-inputring)]]} {
-      lappend errors "Ring does not exist on localhost. User has provided $options(-inputring)"
+      set msg "Ring does not exist on localhost. User has provided "
+      append msg "$options(-inputring)"
+      lappend errors $msg
     }
 
   }
 
 
+  ## @brief Create a new instance with same state
+  #
+  # This creates a new OfflineEVBInputPipeParams whose options have the
+  # same value as the original instance.
+  #
+  # @returns a new OfflineEVBInputPipeParams
+  #
   method clone {} {
     
     # get all of the options and their values and make a dict of them
@@ -80,27 +130,69 @@ snit::type OfflineEVBInputPipeParams {
 
 }
 
+###############################################################################
+###############################################################################
+###############################################################################
 
 
+## @brief Constructs and manages a pipeline for unglomming a data file 
+#
+# This manages data stream will stream a set of data files through the 
+# following pipeline:
+#
+#   cat file0 file1 ... | unglom --id | frag2ring --strip | stdintoring ring
+#
+# A consequence of running data through this pipeline is that the outermost 
+# level built data is split into its building blocks and further that all of the
+# data items that formerly lacked any body headers will be assigned a body
+# header.
+#
+# This expects to receive its configuration parameters via an
+# OfflineEVBInputPipeParams object. Also destruction of this will cause the
+# pipeline to be destroyed.
+#
 snit::type OfflineEVBInputPipeline {
   
-  option -daqroot -default {::InstallRoot::Where}  -validatemethod SetPath
+  option -daqroot -default {::InstallRoot::Where} \
+                  -validatemethod SetPath   ;#< specifies the install path to
+                                            ;#  use
 
+  variable running    false   ;#< indicates whether the pipeline is running
+  variable fd         -1      ;#< the file descriptor for the pipeline
 
-  variable running    false
-  variable fd         -1 
-
+  ## @brief A basic constructor
+  # 
+  # Configures the options from the argument
+  #
+  # @param  args  a list of option-value pairs
+  #
   constructor {args} {
     $self configurelist $args
   }
 
+  ## @brief Destroy the pipeline if running
+  #
   destructor {
     if {$fd > 0} {
       catch "close $fd" msg
     }
   }
 
-  typemethod validateParameters {params context} {
+  ## @brief Validate the parameters that are passed into the 
+  #
+  # @todo Use the OfflineEVBInputPipeParams::validate method instead
+  #
+  # Mostly this demands that there is a certain set of options that can
+  # be queried. It is not necessary that the param object passed in as
+  # an argument is anything more than a snit object with the proper 
+  # options defined. However, it is best to just use what was designed.
+  # 
+  # @param params   an OfflineEVBInputPipeParams object
+  # @param context  name of proc in which this method was called
+  #
+  # @throws error if unglomid is not specified, inputring is not present,
+  #               of file is not present.
+  typemethod validateParameters {params context} { 
      set keys [$params info options]
      if {"-unglomid" ni $keys} {
        set msg "$context : no source id specified for unglom"
@@ -129,13 +221,25 @@ snit::type OfflineEVBInputPipeline {
   }
 
   ## @brief ensure that a path exists before setting it
-
+  #
+  # @throws error if the path doesn't exist
   method SetPath {option value} {
     if {![file exists $value]} {
       return -code error "OfflineEVBInputPipeline::SetPath passed path that does not exist for option: $option"
     } 
   }
 
+  ## @brief Launch the pipeline
+  #
+  # Given a parameter set (OfflineEVBInputPipeParams), create and configure the
+  # pipeline.
+  # 
+  # If the ring that is to be used does not exist, this will simply create it.
+  # Validation of the parameters occurs to ensure that we can abort trying to
+  # create the pipeline if it is destined to fail anyway.
+  #
+  # @param params an OfflineEVBInputPipeParams object
+  #
   method launch {params} {
     if {[catch {ringbuffer usage [$params cget -inputring]} msg]} {
       # failed to find the ring
@@ -148,13 +252,12 @@ snit::type OfflineEVBInputPipeline {
     # Set up the command to run based on the namespace variables
     set cmd [$self _createPipelineCommand $params]
 
-    puts $cmd
-
     # launch the pipeline 
     $self setPipeFD [open $cmd r]
     # update the running state
     $self setRunning true
 
+    # configure the pipeline
     set theFD [$self getPipeFD]
     chan configure $theFD -blocking 0
     chan configure $theFD -buffering line 
@@ -162,6 +265,11 @@ snit::type OfflineEVBInputPipeline {
 
   }
 
+  ## @brief Format the command pipeline using the parameter set
+  #
+  # @param params   an OfflineEVBInputPipeParams object
+  #
+  # @returns the command pipeline 
   method _createPipelineCommand {params} {
 
     set unglomid  [$params cget -unglomid]
@@ -181,24 +289,47 @@ snit::type OfflineEVBInputPipeline {
     return $pipeline
   }
 
+  ## @brief Access the file descriptor
+  #
+  # @returns file descriptor handle
   method getPipeFD {} {
     return $fd
   }
 
+  ## @brief Set the fd handle
+  # 
+  # This really is not meant to be used any more than in testing or 
+  # internally to this method.
+  #
+  # @param newfd  the file handle
   method setPipeFD {newfd} {
     set fd $newfd
   }
 
+  ## @brief Set the running state flag
+  #
+  # Set the flag to indicate whether the pipeline is running or not. Querying
+  # the running state is what enable the user to determine if the pipeline
+  # has exited or not.
+  #
+  # @param yesno  a boolean value to indicate if the state is running
   method setRunning {yesno} {
     set running $yesno
   }
 
+  ## @brief Retrieve running status
+  #
+  # @returns boolean indicating whether the pipeline is active or not
   method getRunning {} {
     return $running
   }
 
-
-
+  ## @brief Callback used when the pipeline is readable
+  #
+  # This manages the running status. Any messages output from the pipeline
+  # are redirected to stdout as normal output. When the pipeline eof is 
+  # set a message is printed and this callback is unregistered.
+  # 
   method _handleReadable {} {
     set theFD [$self getPipeFD]
     set line [read $theFD]
