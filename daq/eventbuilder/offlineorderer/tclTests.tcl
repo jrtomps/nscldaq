c211226a35d06f870f7220bc6bb9bd0f719509a8
Jeromy Tompkins <tompkins@nscl.msu.edu>
2014-09-24 14:42:20 +0000

    Ensure that failed tcltests causes the Jenkins build to fail

--- daq/eventbuilder/offlineorderer/tclTests.tcl
+++ daq/eventbuilder/offlineorderer/tclTests.tcl
@@ -6,4 +6,13 @@ package require tcltest
 ::tcltest::configure -testdir [file dirname [file normalize [info script]]]
 tcltest::testConstraint false 0
 eval ::tcltest::configure $argv
+
+# add hook to access whether tests failed for generating a meaningful return code
+proc ::tcltest::cleanupTestsHook {} {
+  variable numTests
+  set ::exitCode [expr {$numTests(Failed) >0}]
+}
+
 ::tcltest::runAllTests
+
+exit $exitCode
