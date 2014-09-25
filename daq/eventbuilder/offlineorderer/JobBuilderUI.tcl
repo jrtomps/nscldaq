f4b513941568e80fc91bdb20db2c1fe1650c943b
Jeromy Tompkins <tompkins@nscl.msu.edu>
2014-09-25 12:37:30 +0000

    Adds more documentation and adds the missing updateViewFromModel method

--- daq/eventbuilder/offlineorderer/JobBuilderUI.tcl
+++ daq/eventbuilder/offlineorderer/JobBuilderUI.tcl
@@ -16,12 +16,46 @@ package require OfflineEVBGlobalConfigUI
 package require OfflineEVBJob
 
 
+## \defgroup jobbuilder {The Job Builder Package}
+# 
+# A package for managing a set of jobs. It is responsible for providing the
+# user mechanisms for adding, removing, and editing the set of jobs that are
+# being readied for processing.
+#
+# This is implemented using the Model-View-Presenter paradigm as many of the other
+# GUI components in the Offline EVB. The general idea is that the user instantiates
+# presenter object and then all software deals with the presenter whereas the user
+# deals with the view object, which is created by the presenter. The view does not
+# provide any intelligence to the GUI and merely displays data while forwarding 
+# events to the presenter.
+#
+
+
+## @brief The JobBuilderUI View
+#
+# @ingroup jobbuilder
+#
+# The JobBuilderUIView is a simple megawidget that provides a treeview and buttons
+# to add, edit, and remove items from the treeview. The treeview holds a list of jobs
+# that the user intends to manipulate. It forwards its button events to the 
+# presenter object (JobBuilderUIPresenter). 
+#
 snit::widget JobBuilderUIView {
   
-  component m_presenter 
+  component m_presenter         ;#< The presenter
 
-  variable listWidget ""
+  variable listWidget ""        ;#< The treeview widget
 
+  
+  ## @brief Construct object and build the GUI
+  #
+  # This doesn't make the view visible but will handle the gridding of all the 
+  # widgets that make it up. Initially, the object knows nothing about a presenter
+  # and is thus dumb. This is further reason why the user should not be creating 
+  # one of the objects directly. It MUST be paired with a presenter object through
+  # the setPresenter method if it is to do anything useful.
+  # 
+  # @params args  not used...
   constructor {args} {
 
     set m_presenter ""
@@ -33,13 +67,21 @@ snit::widget JobBuilderUIView {
     $self configureStyles
   }
 
+  ## @brief Destroys the widget
+  #
+  # Note that this has not destroy method, but rather is treated just like a normal
+  # widget. It is destroyed as "destroy $widget"
   destructor {
-
   }
 
+
+  ## @brief Build the GUI
+  #
+  # This instantiates all of the widgets that compose the megawidget and grids them
+  # all appropriately.
+  #
   method buildGUI {} {
 
-    # build the 
     set top [ttk::frame $win.listFrame]
     ttk::label $top.flistLbl -text "Jobs To Process" -style "H1.TLabel"
 
@@ -75,107 +117,194 @@ snit::widget JobBuilderUIView {
 
   }
   
-  ##
+  ## @brief Define the text style used to display the "Jobs To Process"
+  # 
+  # We manage the H1.TLabel style here.
   #
   method configureStyles {} {
     ttk::style configure H1.TLabel -font "helvetica 18"
   }
 
+  ## @brief Forward "Add Job" button event to presenter
+  #
   method onAddJob {} {
     $m_presenter addJob
   }
 
+  ## @brief Forward "Edit Selected" button event to presenter
+  #
   method onEditJob {} {
     $m_presenter editJob 
   }
 
+  ## @brief Forward "Remove Selected" button event to presenter
+  #
   method onRemoveJob {} {
     $m_presenter removeJob 
   }
 
-  ##
+  ## @brief Query the name of the presenter object that controls this
   #
+  # @returns the name of the presenter
   method getPresenter {} {
     return $m_presenter
   }
 
-  ##
+  ## @brief Connect a presenter object to control this.
   #
+  # This only knows how to talk to 1 presenter object, but multiple
+  # presenter object can control it. The presenter is not owned by this
+  # in any respect and is merely referenced.
+  #
+  # @param newPresenter a JobBuilderUIPresenter object
   method setPresenter {newPresenter} {
     set m_presenter $newPresenter
   }
 
-  ##
+  ## @brief Retrieve the name of the treeview widget
   #
+  # @returns widget name
   method getTreeWidget {} {
     return $listWidget.list 
   }
 
-  ##
+  ## @brief Insert a new item into the treeview at the end of the list
   #
+  # All added jobs are children of the root node and are appended to 
+  # the very end.
+  #
+  # @param name   the name to display as text and also to use as an id
   method appendEntry {name} {
     [$self getTreeWidget] insert {} end -id $name -text $name
   }
 
+
+  method clear {} {
+    set tree [$self getTreeWidget] 
+    # empty it (all items were children of the root node)
+    $tree delete [$tree children {}]
+  }
 }
 
 # -----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 #
 
+## @brief The presenter object for the JobConfigUI Package
+#
+# @ingroup jobbuilder
+#
+# This is the class that maintains the logic of the display. It owns a 
+# view object (JobBuilderUIView) and manipulates it. It is also responsible
+# for launching dialogues for the user to interact with when configuring
+# the gui.
+#
+# The model that this maintains is a dictionary of jobs. The keys of 
+# the dictionary are the names of the jobs, so that it is not allowed for
+# 2 jobs to have the same job name. The value that is referenced by the key
+# is a dictionary whose keys are: -jobname, -inputparams, -hoistparams, 
+# -evbparams, and -outputparams. These contain enough information for 
+# constructing the entire 4 pipelines that will be used to process a set
+# of files as a job.
+#
+#
 snit::type JobBuilderUIPresenter {
-  option -widgetname     -default ""
+  option -widgetname     -default ""  ;#< The name of the view widget
   
-  variable currentFile    ""
-  
-  component m_view
+  component m_view                    ;#< The view itself
 
-  variable m_masterJobList
-  variable m_useParamsToCreate 
-  variable m_nJobs
+  variable m_masterJobList            ;#< The dictionary of jobs
+  variable m_useParamsToCreate        ;#< Flag that can be set to determine
+                                      ;# whether a job being configured is 
+                                      ;# to be accepted.
+  variable m_nJobs                    ;# A counter to create unique job names
 
-  ## Constructor
+  ## @brief Constructor
   #
+  # THis demands that the name for the view widget is provided and will 
+  # throw an error if not provided. This is important because the presenter
+  # is only really a sensible thing to have if it has a view to control.
+  # the view is thus owned by the presenter.
+  # 
+  # @param  args  option-value pairs 
+  # 
+  # @throws an error if the user didn't provide a value for -widgetname 
   constructor {args} {
 
     # allow the user to override the defaults
     $self configurelist $args
 
+    # verify that the user provided a name for the view
     if {$options(-widgetname) eq ""} {
       set msg    "JobBuilderUIPresenter -widgetname option is mandatory "
       append msg "but was not provided!"
       return -code error $msg 
     }
 
+    # Construct the view and introduce itself to the view for receiving
+    # events
     set m_view [JobBuilderUIView $options(-widgetname)]
     $m_view setPresenter $self 
 
+    # The initial job list is an empty dict
     set m_masterJobList [dict create]
 
+    # Some helper variable.
     set m_useParamsToCreate 0
     set m_nJobs 0
   }
 
-  ## Destructor
+  ## @brief Destroy the view
   #
   destructor {
     catch { destroy $m_view}
   }
 
-  ## Set the model
+  ## @brief Set the model
   #
+  # Replace the current model with a new model. This owns the list of jobs
+  # and is allowed to destroy them all.
   method setModel {newModel} {
     set m_model $newModel
     $self updateViewDataFromModel
   }
 
-  ## Set the view that this controls
+  ## @brief Set the view that this controls
+  #
+  # This replaces any view that was previously being controlled by this. The
+  # ownership of the old view is transfered to the caller.
   #
   method setView {theview} {
+    set prevView $m_view
+
     set m_view $theview
     $self updateViewDataFromModel
+
+    return $prevView
   }
 
-  ## Retrieive the list of jobs
+
+  ## @brief Update the displayed data with the model
+  #
+  method updateViewDataFromModel {} {
+      
+    # clear the entries in the tree (none should be displayed after this)
+    $m_view clear
+
+    # reset counter 
+    set m_nJobs 0
+
+    # get the list of jobs
+    dict for {name job} $m_masterJobList {
+      set newName [$self constructNewJobName $job]
+      $self appendNewJob $newName $job 
+      incr m_nJobs
+    }
+     
+  }
+
+  ## Retrieve the list of jobs
   #
   method getJobsList {} {
     return $m_masterJobList
@@ -224,11 +353,7 @@ snit::type JobBuilderUIPresenter {
       incr m_nJobs
 
     } else {
-      dict for {key val} $model { 
-        if {$key ne "-jobname"} {
-          $val destroy 
-        }
-      }
+      Job::destroy $model
     }
 
 
@@ -347,6 +472,7 @@ snit::type JobBuilderUIPresenter {
     # job parameterization directly.
 
   }
+
   ##
   #
   method acceptCreation {value} {
