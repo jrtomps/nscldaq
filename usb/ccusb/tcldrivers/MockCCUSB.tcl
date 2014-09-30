8a2b910fe9aeb5bb24c72403069cf370eab63913
Jeromy Tompkins <tompkins@nscl.msu.edu>
2014-09-30 14:10:37 +0000

    Puts MockCCUSB into namespace and adds ReadoutList

--- usb/ccusb/tcldrivers/MockCCUSB.tcl
+++ usb/ccusb/tcldrivers/MockCCUSB.tcl
@@ -4,22 +4,57 @@ package provide MockCCUSB 1.0
 
 package require snit
 
+namespace eval MockCCUSB {
 
-snit::type MockCCUSB {
+  snit::type Controller {
 
-  variable m_history
+    variable m_history
 
-  constructor {args} {
+    constructor {args} {
+      set m_history [list]
+    }
+
+    method simpleWrite24 {n a f data} {
+      lappend m_history "simpleWrite24 $n $a $f $data"
+    }
+
+    method simpleRead24 {n a f} {
+      lappend m_history "simpleRead24 $n $a $f"
+    }
+
+    method simpleControl {n a f} {
+      lappend m_history "simpleControl $n $a $f"
+    }
+
+    method getHistory {} {
+      return $m_history
+    }
+  }
+
+
+
+  snit::type ReadoutList {
+    variable m_history
+
+    constructor {args} {
+      set m_history [list]
+    }
+
+
+    method addControl {n a f} {
+      lappend m_history "control $n $a $f"
+    }
+
+    method addRead16 {n a f} {
+      lappend m_history "read16 $n $a $f"
+    }
+
+    method getHistory {} {
+      return $m_history
+    }
 
   }
 
-  method simpleWrite24 {n a f data} {
-    lappend m_history "simpleWrite24 $n $a $f $data"
-  }
 
-
-  method getHistory {} {
-    return $m_history
-  }
 }
 
