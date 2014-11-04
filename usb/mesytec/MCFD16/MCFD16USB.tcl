e7be7356d8016f1c4e7cd51b52c4dd9a3c9f91d4
Jeromy Tompkins <tompkins@nscl.msu.edu>
2014-11-04 17:00:46 +0000

    Adds support for cfd fraction, deadtime, delay, and width

--- usb/mesytec/MCFD16/MCFD16USB.tcl
+++ usb/mesytec/MCFD16/MCFD16USB.tcl
@@ -14,6 +14,7 @@ snit::type MCFD16USB {
   constructor {serialFile args} {
     set m_serialFile [open $serialFile "w+"]
 
+    chan configure $m_serialFile -blocking 0
 #    $self configurelist $args
   }
 
@@ -89,6 +90,70 @@ snit::type MCFD16USB {
     $self Write "ST $ch $thresh"
   }
 
+  method SetWidth {chanPair value} {
+    $self ThrowOnBadChannelPair $chanPair
+
+    if {![Utils::isInRange 16 222 $value]} {
+      set msg {Invalid width argument provided. Must be in range [16,222].}
+      return -code error -errorinfo MCFD16USB::SetWidth $msg
+    }
+
+    $self Write "SW $chanPair $value"
+  }
+
+
+  method SetDeadtime {chanPair value} {
+    $self ThrowOnBadChannelPair $chanPair
+
+    if {![Utils::isInRange 27 222 $value]} {
+      set msg {Invalid deadtime argument provided. Must be in range [27,222].}
+      return -code error -errorinfo MCFD16USB::SetDeadtime $msg
+    }
+
+
+    $self Write "SD $chanPair $value"
+  }
+
+  method SetDelay {chanPair value} {
+
+    $self ThrowOnBadChannelPair $chanPair
+
+    if {![Utils::isInRange 1 5 $value]} {
+      set msg {Invalid delay argument provided. Must be in range [1,5].}
+      return -code error -errorinfo MCFD16USB::SetDelay $msg
+    }
+
+    $self Write "SY $chanPair $value"
+  }
+
+  method SetFraction {chanPair value} {
+
+    $self ThrowOnBadChannelPair $chanPair
+
+    if {$value ni [list 20 40]} {
+      set msg "Invalid fraction argument provided. Must be either 20 or 40."
+      return -code error -errorinfo MCFD16USB::SetFraction $msg
+    }
+
+    $self Write "SF $chanPair $value"
+  }
+
+
+  method SetChannelMask {bank mask} {
+
+    if {$bank ni [list 0 1]} {
+      set msg "Invalid bank parameter provided. Must be either 0 or 1."
+      return -code error -errorinfo MCFD16USB::SetChannelMask $msg
+    }
+
+    if {![Utils::isInRange 0 255 $mask]} {
+      set msg {Invalid mask argument provided. Must be in range [0,255].}
+      return -code error -errorinfo MCFD16USB::SetChannelMask $msg
+    }
+
+    $self Write "SK $bank $mask"
+  }
+
   #---------------------------------------------------------------------------#
   # Utility methods
   #
@@ -96,6 +161,12 @@ snit::type MCFD16USB {
   method Write {script} {
     puts $m_serialFile $script
     chan flush $m_serialFile
+
+  }
+
+  method Read {} {
+    set response [chan read $m_serialFile]
+    return $response
   }
 
   method ThrowOnBadChannelPair {chanPair} {
