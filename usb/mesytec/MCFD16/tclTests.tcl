fdd5cc82a24c6dffd26d29173c235d7fc2f818bb
Jeromy Tompkins <tompkins@nscl.msu.edu>
2014-11-19 14:09:30 +0000

    Adds ability to fail Jenkins to tclTests.tcl

--- usb/mesytec/MCFD16/tclTests.tcl
+++ usb/mesytec/MCFD16/tclTests.tcl
@@ -13,4 +13,13 @@ package require tcltest
 tcltest::testConstraint false 0
 eval ::tcltest::configure $argv
 
+
+# add hook to access whether tests failed for generating a meaningful return code
+proc ::tcltest::cleanupTestsHook {} {
+  variable numTests
+  set ::exitCode [expr {$numTests(Failed) >0}]
+}
+
 ::tcltest::runAllTests 
+
+return $::exitCode
