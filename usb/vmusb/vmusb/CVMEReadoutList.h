a53f0a0e931028cf708dd7f65cb8792b120d34a2
Jeromy Tompkins <tompkins@nscl.msu.edu>
2014-11-20 16:07:49 +0000

    Improves CVMEReadoutList interface

--- usb/vmusb/vmusb/CVMEReadoutList.h
+++ usb/vmusb/vmusb/CVMEReadoutList.h
@@ -10,16 +10,14 @@ class CVMEReadoutList {
     virtual void clear()=0;
     virtual size_t size() const=0;
 
-    virtual void append(const CVMEReadoutList& list)=0;
+    virtual void addRegisterRead(unsigned int address)=0;
+    virtual void addWrite32(uint32_t address, uint8_t amod, uint32_t datum)=0;
+    virtual void addWrite16(uint32_t address, uint8_t amod, uint16_t datum)=0;
+    virtual void addWrite8(uint32_t address, uint8_t amod, uint8_t datum)=0;
 
-    virtual  void addRegisterRead(unsigned int address)=0;
-    virtual  void addWrite32(uint32_t address, uint8_t amod, uint32_t datum)=0;
-    virtual  void addWrite16(uint32_t address, uint8_t amod, uint16_t datum)=0;
-    virtual  void addWrite8(uint32_t address, uint8_t amod, uint8_t datum)=0;
-
-    virtual  void addRead32(uint32_t address, uint8_t amod)=0;
-    virtual  void addRead16(uint32_t address, uint8_t amod)=0;
-    virtual  void addRead8(uint32_t address, uint8_t amod)=0;
+    virtual void addRead32(uint32_t address, uint8_t amod)=0;
+    virtual void addRead16(uint32_t address, uint8_t amod)=0;
+    virtual void addRead8(uint32_t address, uint8_t amod)=0;
 
     virtual void addBlockRead32(uint32_t baseAddress, uint8_t amod, size_t transfers)=0;
     virtual void addFifoRead32(uint32_t baseAddress, uint8_t amod, size_t transfers)=0;
@@ -34,8 +32,8 @@ class CVMEReadoutList {
     virtual void addMaskedCountBlockRead32(uint32_t address, uint8_t amod)=0;
     virtual void addMaskedCountFifoRead32(uint32_t address, uint8_t amod)=0;
 
-    void addDelay(uint8_t clocks)=0;
-    void addMarker(uint16_t value)=0;
+    virtual void addDelay(uint8_t clocks)=0;
+    virtual void addMarker(uint16_t value)=0;
     
 };
 
