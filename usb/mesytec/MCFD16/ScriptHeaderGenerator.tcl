933ce9dc89c333946e82a412038fba637532cd05
Jeromy Tompkins <tompkins@nscl.msu.edu>
2014-12-09 15:12:07 +0000

    Documents the ScriptHeaderGenerator snit::type source code

--- usb/mesytec/MCFD16/ScriptHeaderGenerator.tcl
+++ usb/mesytec/MCFD16/ScriptHeaderGenerator.tcl
@@ -17,15 +17,33 @@ package provide scriptheadergenerator 1.0
 
 package require snit
 
-
+## @brief A snit::type to generate the header portion of saved file
+#
+# Depending on whether the user specified a usb or mxdcrcbus protocol, the 
+# first few lines in the saved file must be different to reflect the different
+# ways to instantiate. This snit::type handles the difference and can determine
+# what the appropriate text to add should be according to the MCFD16AppOptions.
+#
 snit::type ScriptHeaderGenerator {
 
-  variable _options
+  variable _options ;# the MCFD16AppOptions to refer to
 
+  ## @brief Construct and record the options
+  #
+  # @param options  the MCFD16AppOptions instance name manage by MCFD16GuiApp
+  #
+  # @returns name of constructed snit::type
   constructor {options} {
     set _options $options 
   }
 
+  ## @brief Generate a header
+  # 
+  # The user of this snit::type is expected to call this method rather than the
+  # others. Doing so allows the logic of the snit::type to handle how to
+  # properly generate the header according the options.
+  #
+  # @returns list of statements that compose the header
   method generateHeader {} {
     set protocol [$_options cget -protocol]
 
@@ -39,6 +57,12 @@ snit::type ScriptHeaderGenerator {
     return $header
   }
 
+  ## @brief Specialized method for constructing a USB protocol header
+  #
+  # Generates a header that instantiates an MCFD16USB type with the appropriate
+  # serial file name.
+  #
+  # @returns list of statements that compose the header
   method generateUSBHeader {} {
     set header [list]
     lappend header {package require mcfd16usb}
@@ -52,6 +76,13 @@ snit::type ScriptHeaderGenerator {
     return $header
   }
 
+  ## @brief Specialized generator for the mxdcrcbus type protocol
+  #
+  # This will generate statements that when added to a file can be executed to
+  # instantiate an MCFD16RC driver whose proxy is of type MXDCRCProxy. The proxy
+  # receives parameters given by the -host, -port, -module, and -devno options.
+  # 
+  # @returns list of statements that compose the header
   method generateMxDCRCBusHeader {} {
     set header [list]
 
