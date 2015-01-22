b67cd1afd41d44ed425e21baf5540cf2ae995206
Ron Fox <fox@nscl.msu.edu>
2015-01-22 18:51:18 +0000

    Data Acquisition software living on top of dash core.

--- dashdaq/vme/CBlockReadElement.cpp
+++
@@ -1,7 +0,0 @@
-#include <config.h>
-#include "CBlockReadElement.h"
-
-#ifdef HAVE_STD_NAMESPACE
-using namespace std;
-#endif
-
--- dashdaq/vme/CBlockWriteElement.cpp
+++
@@ -1,6 +0,0 @@
-#include <config.h>
-#include "CBlockWriteElement.h"
-
-#ifdef HAVE_STD_NAMESPACE
-using namespace std;
-#endif
