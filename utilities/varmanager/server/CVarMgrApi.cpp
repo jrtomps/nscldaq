7f3cc941525baaa16b82deb60ac776f9af2dbea4
Ron Fox <fox@nscl.msu.edu>
2015-03-10 19:53:00 +0000

    *  API Base class for server functionality. *  CPPUnit test infrastructure.

---
+++ utilities/varmanager/server/CVarMgrApi.cpp
@@ -0,0 +1,167 @@
+/**
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2013.
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#    Author:
+#            Ron Fox
+#            NSCL
+#            Michigan State University
+#            East Lansing, MI 48824-1321
+
+##
+# @file   CVarMgrApi.cpp
+# @brief  Implementation of common utilities for variable manager apis.
+# @author <fox@nscl.msu.edu>
+*/
+#include "CVarMgrApi.h"
+#include <set>
+
+/**
+ * addTransition
+ *   Add a transition to  state map.
+ *   -   If the state is not yet defined, it is created.
+ *   -   If the toState is a duplicate a duplicate transition is registered
+ *       (which is not a fatal thing).
+ *  @param[in,out] map    state transition map that will be modified.
+ *  @param fromState[in]  State from which this transition originates.
+ *  @param toState[in]    State to which this transition takes the state machine.
+ */
+void
+CVarMgrApi::addTransition(
+    StateMap& map, std::string fromState, std::string  toState
+)
+{
+    map[fromState].push_back(toState);
+    
+}
+
+/**
+ * validTransitinoMap
+ *   Determines if a transition map has errors. The following errors are
+ *   detected:
+ *   - Transitions to nonexistent states.
+ *   - Unreachable states
+ *   
+ *   
+ * @param map - The map being checked.
+ * @return bool - true if there are neither transitions to non-existent states
+ *                nor unreachable states.
+ * @note Initial states may be perfectly ok to be unreachable.
+ */
+bool
+CVarMgrApi::validTransitionMap(StateMap map)
+{
+    std::set<std::string> reachableStates;
+    std::set<std::string> states;
+    
+    /* Iterate over the state transitions looking for to states that are not
+     * valid and building up the reachableStates and states sets.
+     */
+     
+    for(StateMap::iterator pState = map.begin(); pState != map.end(); pState++) {
+        states.insert(pState->first);
+        for (int i = 0; i < pState->second.size(); i++) {
+            std::string to = pState->second[i];
+            reachableStates.insert(to);
+            if (map.find(to) == map.end()) {
+                return false;                 // Transition to no such state.
+            }
+        }
+    }
+    /* By now all to states are valid.  If there are states that
+     * are not in the reachable states set, however we can have an
+     * unreachable initial state:
+     */
+    std::set<std::string>::iterator p = states.begin();
+    for(
+        p++;                          // Initial states can be unreachable
+        p != states.end();
+        p++
+    ) {
+        if (reachableStates.count(*p) == 0) {
+            return false;         // Unreachable state.
+        }
+    }
+        // All states are reachable so:
+        
+    return true;
+}
+/**
+ * Utilities for base class members.
+ */
+
+/**
+ * findInvalidTransition
+ *    Return a pair containing the from/to states of an invalid
+ *    transition.
+ *
+ *   @param map  - The map for which to find an invalid transition.
+ *   @return pair<string, string> Consisting of the from to states of a
+ *                 transition to an undefined state.
+ *   @throw CVarMgrApi::CException - if there is no invalid transition.
+ *   
+ */
+std::pair<std::string, std::string>
+CVarMgrApi::findInvalidTransition(CVarMgrApi::StateMap map)
+{
+    for(StateMap::iterator pState = map.begin(); pState != map.end(); pState++) {
+       for (int i = 0; i < pState->second.size(); i++) {
+           std::string to = pState->second[i];
+           if (map.find(to) == map.end()) {
+               return std::pair<std::string, std::string>(pState->first, to);
+           }
+       }
+    }
+    // No invalid states:
+    
+    throw CException("There are no transitions to undefined states");
+    
+}
+/**
+ * findUnreachableState
+ *    Return a string that is the name of a state that has no
+ *    transitions to it.  Such a state is unreachable.  This is
+ *    ok if the state is the initial state but not for any other state.
+ *
+ *  @param map map to check
+ *  @return std::string - name of the first unreachable non-initial state.
+ *  @throw CVarMgrApi::CException - if all states are reachable.
+ */
+std::string
+CVarMgrApi::findUnreachableState(CVarMgrApi::StateMap map)
+{
+    std::set<std::string> reachableStates;
+    std::set<std::string> states;
+    
+    /* Iterate over the state transitions fillig in the state and
+     * reachable state set.
+     */
+     
+    for(StateMap::iterator pState = map.begin(); pState != map.end(); pState++) {
+        states.insert(pState->first);
+        for (int i = 0; i < pState->second.size(); i++) {
+            std::string to = pState->second[i];
+            reachableStates.insert(to);
+        }
+    }
+    /*
+     * Find the first non-reachable non initial state
+     */
+    std::set<std::string>::iterator p = states.begin();
+    for(
+        p++;                          // Initial states can be unreachable
+        p != states.end();
+        p++
+    ) {
+        if (reachableStates.count(*p) == 0) {
+            return *p;
+        }
+    }
+    throw CException("All non initial states are reachable");
+}
---
+++ utilities/varmanager/server/CVarMgrApi.h
@@ -0,0 +1,71 @@
+/**
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2013.
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#    Author:
+#            Ron Fox
+#            NSCL
+#            Michigan State University
+#            East Lansing, MI 48824-1321
+
+##
+# @file   CVarMgrApi.h
+# @brief  Variable database manager client API
+# @author <fox@nscl.msu.edu>
+*/
+#ifndef CVARMGRAPI_H
+#define CVARMGRAPI_H
+
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <map>
+
+
+/**
+ * @class CVarMgrAPI
+ *
+ *   Abstract base class that defines a common interface that can be used
+ *   for either locally connected variable databases or as server clients.
+ *   This API only addresses the functionality in the server REQ/REP
+ *   socket pair.
+ */
+class CVarMgrApi
+{
+public:
+    typedef std::vector<std::string> EnumValues;
+    typedef std::map<std::string, std::vector<std::string> > StateMap;
+public:
+    virtual void cd(const char* path = "/") = 0;
+    virtual void mkdir(const char* path)    = 0;
+    virtual void rmdir(const char* path)    = 0;
+    virtual void declare(const char* path, const char* type, const char* initial=0) = 0;
+    virtual void set(const char* path, const char* value) = 0;
+    virtual std::string get(const char* path) = 0;
+    virtual void defimeEnum(const char* typeName, EnumValues values) = 0;
+    virtual void defineStateMachine(const char* typeName, StateMap transitions) = 0;
+    void addTransition(StateMap& map, std::string fromState, std::string  toState);
+    bool validTransitionMap(StateMap map);
+protected:
+    
+    std::pair<std::string, std::string> findInvalidTransition(StateMap map);
+    std::string                         findUnreachableState(StateMap map);
+    
+    // nested classes:
+public:
+    class CException : public std::runtime_error {
+    public:
+        CException(std::string what) noexcept :
+            runtime_error(what) {}
+        CException(const char* what) noexcept :
+            runtime_error(what) {}    
+    };
+};
+
+#endif
