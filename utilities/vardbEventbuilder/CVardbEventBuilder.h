cb9cb2f9dbeab34f460183776ae41b131efca150
Ron Fox <fox@nscl.msu.edu>
2015-12-08 08:26:11 -0500

    Initial add of file.

---
+++ utilities/vardbEventbuilder/CVardbEventBuilder.h
@@ -0,0 +1,21 @@
+/**
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2013.
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#    Author:
+#            Ron Fox
+#            NSCL
+#            Michigan State University
+#            East Lansing, MI 48824-1321
+
+##
+# @file   CVardbEventBuilder.h
+# @brief  Header for C++ api to maniuplate event builder defs in variable database.
+# @author <fox@nscl.msu.edu>
+*/
