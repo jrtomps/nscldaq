5b894fd69ec8d57917b9646c355b67301c2f90db
Ron Fox <fox@nscl.msu.edu>
2015-12-30 07:01:04 -0500

    Make the event builder application class subclassable.

--- utilities/vardbEventbuilder/PyEventBuilder.cpp
+++ utilities/vardbEventbuilder/PyEventBuilder.cpp
@@ -1367,7 +1367,7 @@ static PyTypeObject vardb_VarDbEvbType = {
     0,                         /*tp_getattro*/
     0,                         /*tp_setattro*/
     0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT,        /*tp_flags*/
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,        /*tp_flags*/
     "Event builder variable database api objects", /* tp_doc */
     0,		               /* tp_traverse */
     0,		               /* tp_clear */
