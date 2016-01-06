1ee52a592eab6b4872f27d75e9263b8279e2d6ec
Ron Fox <fox@nscl.msu.edu>
2016-01-06 07:27:24 -0500

    Feature #4849 - offline scaler sum program *  Add CChannel base class (abstract) *  Add CIncrementalChannel class that knows how to handle incremental scalers.

--- main/utilities/scalersum/CIncrementalChannel.h
+++ main/utilities/scalersum/CIncrementalChannel.h
@@ -15,27 +15,30 @@
 #            East Lansing, MI 48824-1321
 
 ##
-# @file   CChannel.h
-# @brief  Define a class that manages scaler sums.
+# @file   CIncrementalChannel.h
+# @brief  Manage the sum of an incremental scaler channel.
 # @author <fox@nscl.msu.edu>
 */
 
-#ifndef CCHANNEL_H
-#define CCHANNEL_H
+#ifndef CINCREMENTALCHANNEL_H
+#define CINCREMENTALCHANNEL_H
 
+#include "CChannel.h"
 #include <stdint.h>
 
 /**
- * @class
- *     Encapsulates a scaler channel.  This is an abstract base class
- *     because the handling of incremental and non-incremental channels
- *     are so fundamentally different.
+ * @class CIncrementalChannel
+ *     Encapsulates the sum of an incremental scaler channel.
+ *     for this type of channel, the update is a mask and add operation.
  */
-class CChannel
+class CIncrementalChannel : public CChannel
 {
+    uint64_t m_sum;
 public:
-    virtual void update(unsigned value, unsigned width=32) = 0; // update with new value
-    virtual operator uint64_t() = 0;                        // fetch sum.
+    CIncrementalChannel();
+    
+    void update(unsigned value, unsigned width=32);
+    operator uint64_t();
 };
 
 #endif
