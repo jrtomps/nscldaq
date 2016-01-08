9a98f76d8865d366b2e075115da9ebeb40f6325a
Ron Fox <fox@nscl.msu.edu>
2016-01-08 10:25:05 -0500

    Initial template for the scale control megawidget.

---
+++ main/base/tclwidgets/scaleControl.tcl
@@ -0,0 +1,31 @@
+#!/bin/sh
+# -*- tcl -*-
+# The next line is executed by /bin/sh, but not tcl \
+exec tclsh "$0" ${1+"$@"}
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2014.
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#    Authors:
+#             Ron Fox
+#             Jeromy Tompkins 
+#	     NSCL
+#	     Michigan State University
+#	     East Lansing, MI 48824-1321
+
+
+
+##
+# @file scaleControl.tcl
+# @brief Megawidget to control scales of uhm...stuff.
+# @author Ron Fox <fox@nscl.msu.edu>
+#
+
+package provide scaleControl 1.0
+package require Tk
+package require snit
