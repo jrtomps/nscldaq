64c57d7ecfb40c02ab3a68f63ebc5da03f1fd696
Ron Fox <fox@nscl.msu.edu>
2016-01-12 13:14:17 -0500

    Empty file to ensure we commit

---
+++ main/utilities/newscaler/zoomPrompt.tcl
@@ -0,0 +1,27 @@
+#!/bin/sh
+# -*- tcl -*-
+# The next line is executed by /bin/sh, but not tcl \
+exec tclsh "$0" ${1+"$@"}
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2014.
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#    Authors:
+#             Ron Fox
+#             Jeromy Tompkins 
+#	     NSCL
+#	     Michigan State University
+#	     East Lansing, MI 48824-1321
+
+
+
+##
+# @file zoomPrompt.tcl
+# @brief Form/dialog/convenience proc to prompt for custom zoom.
+# @author Ron Fox <fox@nscl.msu.edu>
+#
