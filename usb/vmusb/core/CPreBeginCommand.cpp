1d1bda97b67117639127651f2fb68242c63b3536
fox <fox@nscl.msu.edu>
2016-03-21 07:16:16 -0400

    prebegin -   Skelet5on file.

---
+++ usb/vmusb/core/CPreBeginCommand.cpp
@@ -0,0 +1,28 @@
+/*
+   This software is Copyright by the Board of Trustees of Michigan
+   State University (c) Copyright 2005.
+
+   You may use this software under the terms of the GNU public license
+   (GPL).  The terms of this license are described at:
+
+http://www.gnu.org/licenses/gpl.txt
+
+Author:
+Ron Fox
+NSCL
+Michigan State University
+East Lansing, MI 48824-1321
+*/
+
+/**
+ * @file CPreBeginCommand.cpp
+ * @brief prebegin command implementation.
+ */
+
+#include "CPreBeginCommand.h"
+#include "TCLObjectProcessor.h"
+#include "TCLObject.h"
+#include "CConfiguration.h"
+#include "Globals.h"
+#include "CVMUSBHighLevelController.h"
+
