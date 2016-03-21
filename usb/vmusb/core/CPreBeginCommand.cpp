adcf082eb16120bb8e614eb03010a3b8611e8f60
fox <fox@nscl.msu.edu>
2016-03-21 07:23:07 -0400

    pre begin command
    constructor/destructor initial parts of operator()

--- usb/vmusb/core/CPreBeginCommand.cpp
+++ usb/vmusb/core/CPreBeginCommand.cpp
@@ -20,9 +20,38 @@ East Lansing, MI 48824-1321
  */
 
 #include "CPreBeginCommand.h"
-#include "TCLObjectProcessor.h"
-#include "TCLObject.h"
+#include <TCLObjectProcessor.h>
+#include <TCLObject.h>
+#include <Exception.h>
+
 #include "CConfiguration.h"
 #include "Globals.h"
 #include "CVMUSBHighLevelController.h"
 
+
+#include <stdexcept>
+
+/**
+ * constructor
+ *   @param interp - interpreter the command will be registered under.
+ */
+CPreBeginCommand::CPreBeginCommand(CTCLInterpreter& interp) :
+    CTCLObjectProcessor(interp, "prebegin", true) {}
+    
+/**
+ * destructor
+ */
+CPreBeginCommand::~CPreBeginCommand() {}
+
+/**
+ *  operator() - executes the actual command.
+ *
+ *  @param interp - interpreter executing the command.
+ *  @param objv   - command words.
+ *  @return int   - TCL_OK - command succeeded.  TCL_ERROR - command failed
+ */
+int
+CPreBeginCommand::operator()(CTCLInterpreter& interp, std::vector<CTCLObject>& objv)
+{
+    bindAll(interp, objv);
+}
