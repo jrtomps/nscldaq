d774f5ab940a772909d98ffcb8aa89d595e0e029
Ron Fox <fox@nscl.msu.edu>
2016-04-08 15:52:12 -0400

    Feature #5531 Task # 5506 - Add support for 2 phase pause.
    Good compile on the CPrePauseCommand class.

--- usb/ccusb/core/CPrePauseCommand.cpp
+++ usb/ccusb/core/CPrePauseCommand.cpp
@@ -15,7 +15,96 @@
 #            East Lansing, MI 48824-1321
 
 ##
-# @file   <filename>
-# @brief  <brief description>
+# @file   CPrePauseCommand.cpp
+# @brief  Implement the pre-pause command.
 # @author <fox@nscl.msu.edu>
 */
+
+#include "CPrePauseCommand.h"
+#include <TCLInterpreter.h>
+#include <TCLObject.h>
+
+#include "CCCUSBHighLevelController.h"
+#include <stdexcept>
+#include <Exception.h>
+#include "CRunState.h"
+#include "Globals.h"
+
+/**
+ * constructor
+ *    @param interp - interpreter on which the command is being implemented.
+ */
+CPrePauseCommand::CPrePauseCommand(CTCLInterpreter& interp) :
+    CTCLObjectProcessor(interp, "prepause")
+    {}
+/**
+ * destructor
+ */
+CPrePauseCommand::~CPrePauseCommand() {}
+
+/**
+ * operator()
+ *    Executes the command.
+ *
+ *  @param interp - the interpreter executing the command.
+ *  @param objv   - The command words (inluding the command keyword).
+ *  @return int   - TCL_OK for success, TCL_ERROR for failure.
+ *  @note If TCL_ERROR is returned, the interpreter result is an error message.
+ */
+int
+CPrePauseCommand::operator()(CTCLInterpreter& interp, std::vector<CTCLObject>& objv)
+{
+    bindAll(interp, objv);
+    try {
+        requireExactly(objv, 1, "prebegin takes no command parameters");
+        
+        // Require that the state be active only.
+
+        perform();        
+    }
+    catch (std::string msg) {
+        interp.setResult(msg);
+        return TCL_ERROR;
+    }
+    catch (const char* msg) {
+        interp.setResult(msg);
+        return TCL_ERROR;
+    }
+    catch (CException& e) {
+        interp.setResult(e.ReasonText());
+        return TCL_ERROR;
+    }
+    catch (std::exception& e) {
+        interp.setResult(e.what());
+        return TCL_ERROR;
+    }
+    catch (...) {
+        interp.setResult("prepause - unexpected exception type thrown");
+        return TCL_ERROR;
+    }
+    return TCL_OK;
+}
+/**
+ * perform
+ *   Perform the state checks, do the action and set the state to Pausing:
+ */
+void
+CPrePauseCommand::perform()
+{
+    // State must be active coming in:
+    
+    CRunState* pR = CRunState::getInstance();
+    CRunState::RunState state = pR->getState();
+    
+    if (state == CRunState::Active) {
+        
+    } else {
+        // wrong state:
+        
+        throw std::logic_error("Unable to prepause a run because it's not 'Active'.");
+    }
+    
+    ::Globals::pController->performStopOperations();
+    
+    pR->setState(CRunState::Pausing);
+}
