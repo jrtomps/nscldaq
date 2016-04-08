7aabf3d1de99b64fb9b936ac7af53af77b965f28
Ron Fox <fox@nscl.msu.edu>
2016-04-08 10:35:08 -0400

    Completed initial coding

--- usb/ccusb/core/CPrePauseCommand.h
+++ usb/ccusb/core/CPrePauseCommand.h
@@ -15,7 +15,34 @@
 #            East Lansing, MI 48824-1321
 
 ##
-# @file   <filename>
-# @brief  <brief description>
+# @file   CPrePauseCommand.h
+# @brief  Prepare the system for a pause.
 # @author <fox@nscl.msu.edu>
 */
+
+#ifndef CPREPAUSECOMMAND_H
+#define CPREPAUSECOMMAND_H
+
+#include "TCLObjectProcessor.h"
+
+class CTCLInterpreter;
+class CTCLObject;
+
+/**
+ * @class CPrePauseCommand
+ *     Class to execute the pre-pause operation.  For now this operation
+ *     just invokes the high level controller's  performStopOperations method.
+*/
+class CPrePauseCommand : public CTCLObjectProcessor
+{
+public:
+    CPrePauseCommand(CTCLInterpreter& interp);
+    virtual ~CPrePauseCommand();
+    
+    int operator()(CTCLInterpreter& interp, std::vector<CTCLObject>& objv);
+    void perform();
+    
+};
+#endif
+
+
