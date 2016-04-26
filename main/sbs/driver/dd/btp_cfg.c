28144674e932e1326efe64278ac7611a51daafe7
Ron Fox <fox@nscl.msu.edu>
2016-04-26 14:10:30 -0400

    Remove the __DATE__ macros this is the end of a rather long and sordid tale:
    *  Linux 4.4.0 build scripts for modules make __DATE__ an error.
    *  with the compiler it uses it is possible to turn off this error
       with the flag:  -Wno-error=date-time
    *  This switch is introduced in gcc 4.8
    *  You might think therefore you can just blithely check the compiler
       for the existence of this switch and you can but:
    *  In at least one distro, debian Jessie, the compiler used to build the
       kernel and modules does not support -Wno-error=date-time while the
       default compiler (the one autogen finds without help) does.  This
       causes errors in the driver module compilation for the obvious reasons.

--- main/sbs/driver/dd/btp_cfg.c
+++ main/sbs/driver/dd/btp_cfg.c
@@ -19,7 +19,7 @@
 *****************************************************************************/
 
 #ifndef LINT
-static const char revcntrl[] = "@(#)"__FILE__"  $Revision: 2330 $" __DATE__;
+static const char revcntrl[] = "@(#)"__FILE__"  $Revision: 2330 $";
 #endif  /* LINT */
 static const char driver_version[] = "$Name: 1003v3p1pC 1003v3p1pB $";
 
