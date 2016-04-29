154531676b1ea6525554c62bcc3cfe12978315a2
Jeromy Tompkins <tompkins@nscl.msu.edu>
2016-04-29 10:16:31 -0400

    Expands timeout resolution limits

--- main/sbs/nsclapi/LockingTest.cpp
+++ main/sbs/nsclapi/LockingTest.cpp
@@ -8,6 +8,10 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <algorithm>
+
+#include <poll.h>
+#include <time.h>
 
 using namespace std;
 
@@ -89,20 +93,32 @@ void tryLock_2()
     bool locked;
     time_point<high_resolution_clock> begin, end;
     auto task = [&locked, &begin, &end](){ 
+//      std::cout << "Inside thread" << std::endl;
       begin = high_resolution_clock::now();
       locked = CVMEInterface::TryLock(1); // timeout = 1 sec
       end = high_resolution_clock::now();
     };
 
+//    std::cout << "Before thread" << std::endl;
     // launch thread and let it finish
     std::thread t1(task);
     t1.join();
+//    std::cout << "After thread" << std::endl;
 
     double waitTime = duration<double>(end-begin).count();
 
     // we will say that a test that waited till within 1 ms of the 
     // complete time is sufficient.
-    double expectTime = 1.0 - 0.001;
+    
+    double resolution = getTimeoutResolution();
+
+    // be very lenient with the time. we are measuring timeouts rather than
+    // sleeps. The system just needs to wait for roughly the amount of time
+    // that we are telliong it to timeout. The timeout is specified as 
+    // seconds, and the clock resolution is 1-10 milliseconds on Linux 
+    // machines. I don't care if we timeout after 980 ms if we told it to time
+    // out after 1000 seconds.  The difference is not noticable to the user.
+    double expectTime = 1.0 - 2*resolution;
     std::string expected ("Actual Time >= ");
     expected += std::to_string(expectTime);
 
@@ -119,6 +135,28 @@ void tryLock_2()
            expected, actual);
 }
 
+double getTimeoutResolution() {
+  using namespace std::chrono;
+
+  auto start = high_resolution_clock::now();
+  // wait for 1 millisecond
+  poll(nullptr, 0, 1);
+  auto stop = high_resolution_clock::now();
+  double poll_resolution = duration<double>(stop-start).count();
+//  std::cout << "poll resolution = " << poll_resolution << std::endl;
+
+  struct timespec spec;
+  clock_getres(CLOCK_REALTIME, &spec);
+  double getres_resolution = spec.tv_sec + spec.tv_nsec*1.0e-9;
+//  std::cout << "CLOCK_REALTIME resolution = " << getres_resolution << "(" << spec.tv_sec << "," << spec.tv_nsec << ")" << std::endl;
+
+  clock_getres(CLOCK_MONOTONIC, &spec);
+  double mono_resolution = spec.tv_sec + spec.tv_nsec*1.0e-9;
+//  std::cout << "CLOCK_MONOTONIC resolution = " << mono_resolution << "(" << spec.tv_sec << "," << spec.tv_nsec << ")" << std::endl;
+  return std::max(
+            std::max(
+                std::max(poll_resolution, getres_resolution), mono_resolution), 0.01);
+}
 
 
 
