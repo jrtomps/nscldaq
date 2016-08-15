db84bdf9bfacca561bedcfd85f1aa3a78d2081d5
Ron Fox <fox@nscl.msu.edu>
2016-08-15 09:44:09 -0400

    Placeholder for COutputThread.cpp - I have to do stuff like this in order to be sure I add new files to git.

---
+++ main/daq/eventbuilder/COutputThread.cpp
@@ -0,0 +1,24 @@
+/**
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2013.
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#    Author:
+#            Ron Fox
+#            NSCL
+#            Michigan State University
+#            East Lansing, MI 48824-1321
+
+##
+# @file   COutputThread.h
+# @brief  Implements COutputThread - see the header for description/rationale.
+# @author <fox@nscl.msu.edu>
+*/
+
+#include "COutputThread.h"
+
