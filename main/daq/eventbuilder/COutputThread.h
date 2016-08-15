0c1d5eb34898aaa35b8e681eb5e3ae7e0781f829
Ron Fox <fox@nscl.msu.edu>
2016-08-15 09:19:16 -0400

    Placeholder for output thread for ordered events

---
+++ main/daq/eventbuilder/COutputThread.h
@@ -0,0 +1,26 @@
+/**
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2013.
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#    Author:
+#            Ron Fox
+#            NSCL
+#            Michigan State University
+#            East Lansing, MI 48824-1321
+
+##
+# @file   COutputThread.h
+# @brief  Thread to for ordered output.
+# @author <fox@nscl.msu.edu>
+*/
+#ifndef COUTPUTTHREAD_H
+#define COUTPUTTHREAD_H
+
+#endif
+
