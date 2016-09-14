f2b831e563730f488ff15b53041edbe7022d07dc
Ron Fox <fox@nscl.msu.edu>
2016-09-14 15:48:14 -0400

    Fix error in Usage string vmusloader -> vmusbloader

--- main/usb/loaders/vmusb_flash.cpp
+++ main/usb/loaders/vmusb_flash.cpp
@@ -26,7 +26,7 @@ int main(int argc, char** argv)
     
     if ((argc < 2) || (argc > 3)) {
         printf("\nUsage:\n");
-        printf("    vmusloader  firmware-file [serial-string]\n");
+        printf("    vmusbloader  firmware-file [serial-string]\n");
         printf("Where:\n");
         printf("     firmware-file - is a Xilinx .bit or .bin formatted firmware file\n");
         printf("     serial-string - if present is the serial number of the VM-USB to flash\n");
