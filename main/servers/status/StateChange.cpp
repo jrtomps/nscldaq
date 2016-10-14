9244160bb55f8634e5668f0c592e10813156f460
Ron Fox <fox@nscl.msu.edu>
2016-10-14 15:28:03 -0400

    Task #6269  - API for creating status messages.
    *   Complete coding of State change message emission.
    *   Complete tests for state change status messagse.

---
+++ main/servers/status/StateChange.cpp
@@ -0,0 +1,101 @@
+/**
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2013.
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#    Author:
+#            Ron Fox
+#            NSCL
+#            Michigan State University
+#            East Lansing, MI 48824-1321
+
+##
+# @file   StateChange.cpp
+# @brief  Emit State change status messages.
+# @author <fox@nscl.msu.edu>
+*/
+
+#include "CStatusMessage.h"
+#include <ctime>
+#include <cstring>
+#include <zmq.hpp>
+#include <stdexcept>
+
+/**
+ * CStatusDefinitions::StateChange::StateChange (constructor)
+ *    Construct an object that can emit state changes on a ZMQ socket.
+ *
+ *  @param socket - References the socket that is used to transport
+ *                  the information.
+ *  @param app    - Name of the application.
+ */
+CStatusDefinitions::StateChange::StateChange(
+    zmq::socket_t& socket, std::string app
+) :
+    m_socket(socket), m_application(app)
+{}
+/**
+ * StatusDefinitions::StateChange::~StateChange
+ *
+ *   Destroy the object.
+ */
+CStatusDefinitions::StateChange::~StateChange() {}
+
+/**
+ * CStatusDefinitions::StateChange::logChange
+ *    Log a state change
+ *    - Emits a header of the appropriate type with INFO severity.
+ *    - Formats and emits a StateChangeBody message.
+ *  @param leaving  - The state that's being left.
+ *  @param entering - The state that's being entered.
+ */
+void
+CStatusDefinitions::StateChange::logChange(
+    std::string leaving, std::string entering
+)
+{
+    // The strings must be appropriately sized:
+    
+    
+    // Format the header
+    
+    Header h = formatHeader(
+        MessageTypes::STATE_CHANGE, SeverityLevels::INFO, m_application.c_str()
+    );
+    
+    // Format the body:
+    
+    StateChangeBody body;
+    
+    // Strings must fit in state slots:
+    
+    if (leaving.size() >= sizeof(body.s_leaving)) {
+        throw std::logic_error("Leaving state is too long for status message");
+    }
+    if (entering.size() >= sizeof(body.s_entering)) {
+        throw std::logic_error("Entering state is too long for status message");
+    }
+    
+    body.s_tod = std::time(nullptr);
+    std::strcpy(body.s_leaving, leaving.c_str());
+    std::strcpy(body.s_entering, entering.c_str());
+    
+    // Turn the structs into zmq messages::
+    
+    zmq::message_t hMsg(sizeof(Header));
+    zmq::message_t bodyMsg(sizeof(body));
+    
+    std::memcpy(hMsg.data(), &h, sizeof(Header));
+    std::memcpy(bodyMsg.data(), &body, sizeof(StateChangeBody));
+    
+    // Send the messages:
+    
+    m_socket.send(hMsg, ZMQ_SNDMORE);
+    m_socket.send(bodyMsg, 0);
+    
+}
--- main/servers/status/statemsgtests.cpp
+++ main/servers/status/statemsgtests.cpp
@@ -2,27 +2,107 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include <os.h>
+#include <zmq.hpp>
 
 
+#define private public
+#include "CStatusMessage.h"
+#undef private
 
-class Testname : public CppUnit::TestFixture {
-  CPPUNIT_TEST_SUITE(Testname);
-  CPPUNIT_TEST(aTest);
+
+static std::string uri = "inproc://test";
+static std::string app = "TestApp";
+
+class StateTests : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(StateTests);
+  CPPUNIT_TEST(construct);
+  CPPUNIT_TEST(message);
+  // CPPUNIT_TEST(leavingTooLong);
+  // CPPUNIT_TEST(enteringTooLong);
   CPPUNIT_TEST_SUITE_END();
 
 
 private:
-
+  zmq::context_t*  m_pContext;
+  zmq::socket_t*   m_pSender;
+  zmq::socket_t*   m_pReceiver;
+  CStatusDefinitions::StateChange* m_pTestObject;
 public:
   void setUp() {
+    m_pContext = new zmq::context_t(1);
+    
+    // Create an internal push/pull pair between sender/receiver:
+    
+    m_pSender   = new zmq::socket_t(*m_pContext, ZMQ_PUSH);
+    m_pReceiver = new zmq::socket_t(*m_pContext, ZMQ_PULL);
+    
+    m_pReceiver->bind(uri.c_str());
+    m_pSender->connect(uri.c_str());
+    
+    // Create an object using the sender socket:
+    
+    m_pTestObject = new CStatusDefinitions::StateChange(*m_pSender, app);
   }
   void tearDown() {
+    delete m_pTestObject;
+    delete m_pSender;
+    delete m_pReceiver;
+    delete m_pContext;
+    m_pContext = 0;
   }
 protected:
-  void aTest();
+  void construct();
+  void message();
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(Testname);
+CPPUNIT_TEST_SUITE_REGISTRATION(StateTests);
 
-void Testname::aTest() {
+// Construction should fill in past elements.
+
+void StateTests::construct() {
+  EQ(m_pSender, &(m_pTestObject->m_socket));
+  EQ(app, m_pTestObject->m_application);
+}
+
+
+// Send and receive the message:
+
+void StateTests::message()
+{
+  m_pTestObject->logChange("NotReady", "Readying");
+  
+  // Should have two message parts:
+  
+  zmq::message_t hMsg;
+  zmq::message_t bMsg;
+  int            haveMore;
+  size_t         s;
+  
+  m_pReceiver->recv(&hMsg);
+  m_pReceiver->getsockopt(ZMQ_RCVMORE, &haveMore, &s);
+  ASSERT(haveMore);
+  
+  m_pReceiver->recv(&bMsg);
+  m_pReceiver->getsockopt(ZMQ_RCVMORE, &haveMore, &s);
+  ASSERT(!haveMore);
+  
+  // Analyze the header:
+  
+  CStatusDefinitions::Header* h =
+    reinterpret_cast<CStatusDefinitions::Header*>(hMsg.data());
+  EQ(CStatusDefinitions::MessageTypes::STATE_CHANGE, h->s_type);
+  EQ(CStatusDefinitions::SeverityLevels::INFO, h->s_severity);
+  EQ(app, std::string(h->s_application));
+  EQ(Os::hostname(), std::string(h->s_source));
+  
+  // Analyze the body (ignore tod).
+  
+  CStatusDefinitions::StateChangeBody* pBody =
+    reinterpret_cast<CStatusDefinitions::StateChangeBody*>(bMsg.data());
+    
+  EQ(std::string("NotReady"), std::string(pBody->s_leaving));
+  EQ(std::string("Readying"), std::string(pBody->s_entering));
+  
 }
