47238a0963a88d513148fdbbcb58e529c44c930a
Ron Fox <fox@nscl.msu.edu>
2016-10-20 09:28:48 -0400

    Task #6269 - API to emit status messages (Tcl)
    Completed testing of the StateChangeMessage tcl wrapper.

--- main/servers/status/tclstatemsgtests.cpp
+++ main/servers/status/tclstatemsgtests.cpp
@@ -29,7 +29,14 @@ class TclStateMsgTests : public CppUnit::TestFixture {
   
   // Destruction tests:
   
+  CPPUNIT_TEST(destroy);
+  CPPUNIT_TEST(destroyNoSuch);
+  
   // Sending messsages.
+  
+  CPPUNIT_TEST(log);
+  CPPUNIT_TEST(logNoToState);
+  CPPUNIT_TEST(logTooMany);
   
   CPPUNIT_TEST_SUITE_END();
 
@@ -67,6 +74,13 @@ protected:
   void construct();
   void constructNoApp();
   void constructTooMany();
+  
+  void destroy();
+  void destroyNoSuch();
+  
+  void log();
+  void logNoToState();
+  void logTooMany();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(TclStateMsgTests);
@@ -111,4 +125,134 @@ void TclStateMsgTests::constructTooMany()
     m_pInterpObj->Eval(construct.str()),
     CTCLException
   );
+}
+// Destruction should remove a command name.
+
+void TclStateMsgTests::destroy()
+{
+  std::string newCmd;
+  std::stringstream construct;
+  construct << "StateChange create " << uri << " " << app;
+  
+  CPPUNIT_ASSERT_NO_THROW(
+    newCmd = m_pInterpObj->Eval(construct.str())
+  );
+  
+  std::stringstream info;
+  info << "info commands " << newCmd;
+  EQ(newCmd, m_pInterpObj->Eval(info.str()));
+  
+  std::stringstream destroy;
+  destroy << "StateChange destroy " << newCmd;
+  CPPUNIT_ASSERT_NO_THROW(
+    m_pInterpObj->Eval(destroy.str())
+  );
+  
+  // The command should no longer be there:
+  
+  EQ(std::string(""), m_pInterpObj->Eval(info.str()));
+}
+
+// Destroying a command that does not exist is an error:
+
+void TclStateMsgTests::destroyNoSuch()
+{
+  CPPUNIT_ASSERT_THROW(
+    m_pInterpObj->Eval("StateChange destroy no-such-object"),
+    CTCLException
+  );
+}
+
+// Produce s state change message from foo to bar.  Should get a message
+// with a header and a StateChangeBody.
+
+void TclStateMsgTests::log()
+{
+  std::string newCmd;
+  std::stringstream construct;
+  construct << "StateChange create " << uri << " " << app;
+  
+  CPPUNIT_ASSERT_NO_THROW(
+    newCmd = m_pInterpObj->Eval(construct.str())
+  );
+  
+  std::stringstream log;
+  log << newCmd << " logChange foo bar";
+  
+  CPPUNIT_ASSERT_NO_THROW(
+    m_pInterpObj->Eval(log.str())
+  );
+  
+  // Read the message parts:
+  
+  zmq::message_t hdr;
+  zmq::message_t body;
+  int64_t        more(0);
+  size_t         s(sizeof(more));
+  
+  m_pReceiver->recv(&hdr);
+  m_pReceiver->getsockopt(ZMQ_RCVMORE, &more, &s);
+  ASSERT(more);
+  
+  m_pReceiver->recv(&body);
+  m_pReceiver->getsockopt(ZMQ_RCVMORE, &more, &s);
+  ASSERT(!more);
+  
+  // Check the header.  Since everyone uses a common format header we'll just
+  // check the type:
+  
+  CStatusDefinitions::Header* pHdr =
+    reinterpret_cast<CStatusDefinitions::Header*>(hdr.data());
+    
+  EQ(CStatusDefinitions::MessageTypes::STATE_CHANGE, pHdr->s_type);
+  
+  // Check the body:
+  
+  CStatusDefinitions::StateChangeBody* pBody =
+    reinterpret_cast<CStatusDefinitions::StateChangeBody*>(body.data());
+    
+  EQ(std::string("foo"), std::string(pBody->s_leaving));
+  EQ(std::string("bar"), std::string(pBody->s_entering));
+}
+// If there are too few parameters that's an error.
+
+void TclStateMsgTests::logNoToState()
+{
+  std::string newCmd;
+  std::stringstream construct;
+  construct << "StateChange create " << uri << " " << app;
+  
+  CPPUNIT_ASSERT_NO_THROW(
+    newCmd = m_pInterpObj->Eval(construct.str())
+  );
+  
+  std::stringstream log;
+  log << newCmd << " logChange foo";
+  
+  CPPUNIT_ASSERT_THROW(
+    m_pInterpObj->Eval(log.str()),
+    CTCLException
+  );
+}
+
+
+// Too many parameters to a log fails too:
+
+void TclStateMsgTests::logTooMany()
+{
+  std::string newCmd;
+  std::stringstream construct;
+  construct << "StateChange create " << uri << " " << app;
+  
+  CPPUNIT_ASSERT_NO_THROW(
+    newCmd = m_pInterpObj->Eval(construct.str())
+  );
+  
+  std::stringstream log;
+  log << newCmd << " logChange foo bar extra-parameter";
+  
+  CPPUNIT_ASSERT_THROW(
+    m_pInterpObj->Eval(log.str()),
+    CTCLException
+  ); 
 }
