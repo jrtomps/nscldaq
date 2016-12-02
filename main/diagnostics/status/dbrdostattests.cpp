cc1a8bfd7a0d5972049b7e7ee0b10a0110b4f080
Ron Fox <fox@nscl.msu.edu>
2016-12-02 16:52:23 -0500

    *  Fix error in logmodel.test that prevented it from runing oot. *  Some but not all tests yet for Readout stats insertion API.

--- main/diagnostics/status/dbrdostattests.cpp
+++ main/diagnostics/status/dbrdostattests.cpp
@@ -2,27 +2,231 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include <CStatusMessage.h>
+
+#define private public
+#include "CStatusDb.h"
+#undef private
+
+#include <CSqlite.h>
+#include <CSqliteStatement.h>
 
 
 
-class Testname : public CppUnit::TestFixture {
-  CPPUNIT_TEST_SUITE(Testname);
-  CPPUNIT_TEST(aTest);
+class RdoDbStatTests : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(RdoDbStatTests);
+  CPPUNIT_TEST(programAndRun);
+  CPPUNIT_TEST(programAndTwoRuns);
+  CPPUNIT_TEST(twoprogramswithruns);
+  CPPUNIT_TEST(programwithstats);
+//  CPPUNIT_TEST(programwithtwostats);
+  // CPPUNIT_TEST(twoprogramswithstats);
+
   CPPUNIT_TEST_SUITE_END();
 
 
 private:
-
+  CStatusDb* m_db;
 public:
   void setUp() {
+    m_db = new CStatusDb(":memory:", CSqlite::readwrite);
   }
   void tearDown() {
+    delete m_db;
   }
 protected:
-  void aTest();
+  void programAndRun();
+  void programAndTwoRuns();
+  void twoprogramswithruns();
+  void programwithstats();
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(Testname);
+CPPUNIT_TEST_SUITE_REGISTRATION(RdoDbStatTests);
 
-void Testname::aTest() {
+
+// Only insert a single program and run info record associated with it.
+
+void RdoDbStatTests::programAndRun() {
+  m_db->addReadoutStatistics(
+    CStatusDefinitions::SeverityLevels::INFO, "Readout", "charlie.nscl.msu.edu",
+    123456, 10, "A test title"
+  );
+  // This should have created a readout_program record and a run_info record
+  // linked back to it -- the inner join query below should return both in one
+  // fell swoop:
+  
+  CSqliteStatement getInfo(
+    m_db->m_handle,
+    "SELECT p.id, p.name, p.host, r.readout_id, r.start, r.run, r.title           \
+      FROM readout_program AS p                                             \
+      INNER JOIN run_info AS r ON p.id = r.readout_id                       \
+    "
+  );
+  ++getInfo;
+  ASSERT(!getInfo.atEnd());
+  EQ(1, getInfo.getInt(0));        // p.id
+  EQ(
+    std::string("Readout"),
+    std::string(reinterpret_cast<const char*>(getInfo.getText(1)))
+  );                              // p.name
+  EQ(
+    std::string("charlie.nscl.msu.edu"),
+    std::string(reinterpret_cast<const char*>(getInfo.getText(2)))
+  );                             // p.host
+  EQ(1, getInfo.getInt(3));      // r.readout_id (FK)
+  EQ(123456, getInfo.getInt(4)); // r.start
+  EQ(10, getInfo.getInt(5));     // r.run
+  EQ(
+    std::string("A test title"),
+    std::string(reinterpret_cast<const char*>(getInfo.getText(6)))
+  );                             // r.title
+  
+  
+  ++getInfo;                                   // should only be one record.
+  ASSERT(getInfo.atEnd());
+}
+//   Insert a program and two runs associated with it.
+
+void RdoDbStatTests::programAndTwoRuns()
+{
+  m_db->addReadoutStatistics(
+    CStatusDefinitions::SeverityLevels::INFO, "Readout", "charlie.nscl.msu.edu",
+    123456, 10, "A test title"
+  );
+  m_db->addReadoutStatistics(
+    CStatusDefinitions::SeverityLevels::INFO, "Readout", "charlie.nscl.msu.edu",
+    124456, 11, "The second run"
+  );
+  
+  // Fetch this out but ordered by the id in the run_info table:
+  
+  CSqliteStatement getInfo(
+    m_db->m_handle,
+    "SELECT p.id, p.name, p.host, r.readout_id, r.start, r.run, r.title     \
+      FROM readout_program AS p                                             \
+      INNER JOIN run_info AS r ON p.id = r.readout_id                       \
+      ORDER BY r.id ASC                                                     \
+    "
+  );
+  
+  // First record is run 10 .. just spot check the run info.
+  
+  ++getInfo;
+  ASSERT(!getInfo.atEnd());
+  
+  EQ(1, getInfo.getInt(3));             // r.readout_id
+  EQ(123456, getInfo.getInt(4));        // r.start
+  EQ(10, getInfo.getInt(5));            // r.run
+  EQ(
+    std::string("A test title"),
+    std::string(reinterpret_cast<const char*>(getInfo.getText(6)))
+  );
+  
+  // Second record is run 11
+  
+  ++getInfo;
+  ASSERT(!getInfo.atEnd());
+
+  EQ(1, getInfo.getInt(3));             // r.readout_id
+  EQ(124456, getInfo.getInt(4));        // r.start
+  EQ(11, getInfo.getInt(5));            // r.run
+  EQ(
+    std::string("The second run"),
+    std::string(reinterpret_cast<const char*>(getInfo.getText(6)))
+  );
+  
+  
+  // there is no third run:
+  
+  ++getInfo;
+  ASSERT(getInfo.atEnd());
+  
+  
+  
+}
+void RdoDbStatTests::twoprogramswithruns()
+{
+  m_db->addReadoutStatistics(
+    CStatusDefinitions::SeverityLevels::INFO, "Readout", "charlie.nscl.msu.edu",
+    123456, 10, "A test title"
+  );
+  m_db->addReadoutStatistics(                   // Distinct readout program.
+    CStatusDefinitions::SeverityLevels::INFO, "Readout", "spdaq20.nscl.msu.edu",
+    124456, 11, "The second run"
+  );
+  
+  // Fetch this out but ordered by the id in the run_info table:
+  
+  CSqliteStatement getInfo(
+    m_db->m_handle,
+    "SELECT p.id, p.name, p.host, r.readout_id, r.start, r.run, r.title     \
+      FROM readout_program AS p                                             \
+      INNER JOIN run_info AS r ON p.id = r.readout_id                       \
+      ORDER BY r.id ASC                                                     \
+    "
+  );
+  // First we should get the one for Readout@charlie.nscl.msu.edu:
+  
+  ++getInfo;
+  ASSERT(!getInfo.atEnd());
+  
+  EQ(1, getInfo.getInt(3));      // r.readout_id points back to frist program.
+  EQ(123456, getInfo.getInt(4)); // r.start
+  EQ(10, getInfo.getInt(5));     // r.run.
+  
+  // Second we should get one for Readout@spdaq20.nscl.msu.edu
+  
+  ++getInfo;
+  ASSERT(!getInfo.atEnd());
+  EQ(2, getInfo.getInt(3));      // r.readout_id points back to second program.
+  EQ(124456, getInfo.getInt(4)); // r.start
+  EQ(11, getInfo.getInt(5));     // r.run.
+  
+  
+  
+  // There should not be a third one:
+  
+  ++getInfo;
+  ASSERT(getInfo.atEnd());
+}
+// Create a readout program, a run and a statistics entry for it:
+
+void RdoDbStatTests::programwithstats()
+{
+  CStatusDefinitions::ReadoutStatCounters stats = {
+    123466, 10, 150, 100, 1024  
+  };
+  m_db->addReadoutStatistics(
+    CStatusDefinitions::SeverityLevels::INFO, "Readout", "charlie.nscl.msu.edu",
+    123456, 10, "A test title", &stats
+  );
+  // We can get all of the information combined in one grand select:
+  
+  CSqliteStatement getInfo(
+    m_db->m_handle,
+    "SELECT r.run, st.run_id, st.readout_id, st.timestamp, st.elapsedtime,    \
+            st.triggers, st.events, st.bytes                              \
+            FROM readout_statistics AS st                                 \
+            INNER JOIN readout_program AS p ON p.id = st.readout_id       \
+            INNER JOIN run_info AS r ON r.id = st.run_id                  \
+    "
+  );
+  // The query should give a single row result -- note that if the records
+  // were not properly linked, the inner join will result in no records returned.
+  
+  ++getInfo;
+  ASSERT(!getInfo.atEnd());
+  
+  EQ(10, getInfo.getInt(0));              // r.run
+  EQ(1, getInfo.getInt(1));               // st.run_id  -- link to run table
+  EQ(1, getInfo.getInt(2));               // st.readout_id - link to readout table
+  EQ(123466, getInfo.getInt(3));          // st.timestamp
+  EQ(10, getInfo.getInt(4));              // st.elapsedtime.
+  EQ(150, getInfo.getInt(5));             // st.triggers.
+  EQ(100, getInfo.getInt(6));             // st.events.
+  EQ(1024, getInfo.getInt(7));            // st.bytes.
+  
+  ++getInfo;
+  ASSERT(getInfo.atEnd());
 }
